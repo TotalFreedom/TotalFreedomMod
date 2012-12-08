0589ebcf480b032f536697efd8b72542795cc40e
Steven Lawson <steven@smlawson.com>
2012-12-07 19:37:27 -0500

    Added /deafen

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_deafen.java
@@ -0,0 +1,48 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import java.util.Random;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = ADMIN_LEVEL.SENIOR, source = SOURCE_TYPE_ALLOWED.BOTH, block_host_console = true, ignore_permissions = false)
+public class Command_deafen extends TFM_Command
+{
+    private static final Random random = new Random();
+    public static final double STEPS = 10.0;
+
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        for (final Player p : server.getOnlinePlayers())
+        {
+            for (double percent = 0.0; percent <= 1.0; percent += (1.0 / STEPS))
+            {
+                final float pitch = (float) (percent * 2.0);
+
+                server.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        p.playSound(randomOffset(p.getLocation(), 5.0), Sound.values()[random.nextInt(Sound.values().length)], 100.0f, pitch);
+                    }
+                }, Math.round(20.0 * percent * 2.0));
+            }
+        }
+
+        return true;
+    }
+
+    private static Location randomOffset(Location a, double magnitude)
+    {
+        return a.clone().add(randomDoubleRange(-1.0, 1.0) * magnitude, randomDoubleRange(-1.0, 1.0) * magnitude, randomDoubleRange(-1.0, 1.0) * magnitude);
+    }
+
+    private static Double randomDoubleRange(double min, double max)
+    {
+        return min + (random.nextDouble() * ((max - min) + 1.0));
+    }
+}
