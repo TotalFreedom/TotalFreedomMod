146c2e0f52137110c4d9734cb02f5dd246fc3e5a
Wilee999 <Infinitywilee@rocketmail.com>
2013-05-16 14:09:15 -0600

    oops

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_banappeal.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_banappeal.java
@@ -12,7 +12,7 @@ public class Command_banappeal extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-      playerMsg("How to get unbanned if you get banned (or you want a friend unbanned).", ChatColor.GOLD);
+        playerMsg("How to get unbanned if you get banned (or you want a friend unbanned).", ChatColor.GOLD);
     	playerMsg("First of all, open up your internet browser and go to totalfreedom.boards.net", ChatColor.AQUA);
     	playerMsg("You will be greeted with some screen saying you have to login, sign up for the forums.", ChatColor.AQUA);
     	playerMsg("After you sign up, login to the forums and go to the Ban or Admin Suspension Appeals section.", ChatColor.AQUA);
@@ -21,7 +21,7 @@ public class Command_banappeal extends TFM_Command
     	playerMsg("Put in a title, and for the message, paste mark's example. Edit the example to fill out your app, make sure the info is valid. If you have broke a rule, don't say 'I got banned for no reason!' Admit it and apologize and maybe you will be unbanned.", ChatColor.AQUA);
     	playerMsg("It is recommended you put this in a notepad incase you forget this info once you are banned.", ChatColor.RED);
     	
-		return senderIsConsole;
+	return senderIsConsole;
     }
 
 }
