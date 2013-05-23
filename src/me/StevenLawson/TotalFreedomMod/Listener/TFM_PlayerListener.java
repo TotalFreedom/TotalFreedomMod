516b08709e9a417f5d759d4f784851039d3959c2
Wilee999 <Infinitywilee@rocketmail.com>
2013-05-23 14:24:00 -0600

    Added /nuke to the tempban-command thing list.

--- src/me/StevenLawson/TotalFreedomMod/Listener/TFM_PlayerListener.java
+++ src/me/StevenLawson/TotalFreedomMod/Listener/TFM_PlayerListener.java
@@ -491,6 +491,14 @@ public class TFM_PlayerListener implements Listener
                 block_command = true;
             }
         }
+        else if (Pattern.compile("^/nuke").matcher(command).find())
+        {
+            if (!TFM_SuperadminList.isUserSuperadmin(p))
+            {
+                block_command = true;
+            }
+        }
+        
         else if (Pattern.compile("^/save-").matcher(command).find())
         {
             if (!TFM_SuperadminList.isUserSuperadmin(p))
