460c2dac83887b00c2f8c005f17ad11cbfa58c22
Dan Jones <wicked@wickedgaminguk.com>
2013-06-09 21:38:32 +0200

    Added creative -a
    Per request from TotalFreedom Forums (http://www.totalfreedom.boards.net/thread/4632/setting-all-players-specified-gamemode) I've added in creative -a.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_creative.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_creative.java
@@ -1,6 +1,9 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_SuperadminList;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -8,41 +11,48 @@ import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.OP, source = SourceType.BOTH)
 @CommandParameters(description = "Quickly change your own gamemode to creative, or define someone's username to change theirs.", usage = "/<command> [partialname]")
-public class Command_creative extends TFM_Command
-{
+public class Command_creative extends TFM_Command {
     @Override
-    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
-    {
-        if (senderIsConsole)
-        {
-            if (args.length == 0)
-            {
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole) {
+        
+        if (senderIsConsole) {
+            if (args.length == 0) {
                 sender.sendMessage("When used from the console, you must define a target user to change gamemode on.");
                 return true;
             }
         }
-
+        
         Player p;
-        if (args.length == 0)
-        {
-            p = sender_p;
+        p = sender_p;
+        if (args.length != 0){
+        if (args[0].equalsIgnoreCase("-a")) {
+           if (!TFM_SuperadminList.isUserSuperadmin(sender)) {
+               sender.sendMessage(TotalFreedomMod.MSG_NO_PERMS);
+               return true;
+           }
+           
+            for (Player player : server.getOnlinePlayers()) {
+                if(!TFM_SuperadminList.isUserSuperadmin(p)) {
+                player.setGameMode(GameMode.CREATIVE);
+                }
+            }
+            
+            Bukkit.broadcastMessage(ChatColor.AQUA + sender.getName() + " - Setting all non-admins gamemode to creative.");
+               return true;
         }
-        else
-        {
-            if (senderIsConsole || TFM_SuperadminList.isUserSuperadmin(sender))
-            {
-                try
-                {
+        }
+        
+        else {
+            if (senderIsConsole || TFM_SuperadminList.isUserSuperadmin(sender)) {
+                try {
                     p = getPlayer(args[0]);
                 }
-                catch (CantFindPlayerException ex)
-                {
+                catch (CantFindPlayerException ex) {
                     sender.sendMessage(ex.getMessage());
                     return true;
                 }
             }
-            else
-            {
+            else {
                 playerMsg("Only superadmins can change other user's gamemode.");
                 return true;
             }
@@ -51,7 +61,8 @@ public class Command_creative extends TFM_Command
         playerMsg("Setting " + p.getName() + " to game mode 'Creative'.");
         playerMsg(p, sender.getName() + " set your game mode to 'Creative'.");
         p.setGameMode(GameMode.CREATIVE);
-
+        
+        
         return true;
     }
 }
