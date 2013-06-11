117fe1e5a71073f5081551beb2490b42772f7194
Wilee999 <Infinitywilee@rocketmail.com>
2013-06-11 02:09:51 -0600

    Fix formatting (attempt 2)

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_griefer.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_griefer.java
@@ -12,21 +12,21 @@ import org.bukkit.entity.Player;
 @CommandParameters(description = "Report a griefer", usage = "/<command> [message]", aliases = "report")
 public class Command_griefer extends TFM_Command
 {
-        @Override
-	public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
-	{
-		if (args.length == 0)
-		{
-		        TFM_Util.adminbcastMsg(sender.getName() + " is getting griefed!", ChatColor.RED);
-			return true;
-		}
+   @Override
+   public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+   {
+     if (args.length == 0)
+     {
+       TFM_Util.adminbcastMsg(sender.getName() + " is getting griefed!", ChatColor.RED);
+       return true;
+       }
 		
-		String message = StringUtils.join(args, " ");
+       String message = StringUtils.join(args, " ");
 		
-		TFM_Util.adminbcastMsg(String.format("[GrieferReport:%s] %s", sender.getName(), message), ChatColor.RED);
+       TFM_Util.adminbcastMsg(String.format("[GrieferReport:%s] %s", sender.getName(), message), ChatColor.RED);
 		
-		return true;
-		//On-request: Credit to mrnintendofan for the idea!
-	}
+       return true;
+        //On-request: Credit to mrnintendofan for the idea!
+   }
 	
 }
