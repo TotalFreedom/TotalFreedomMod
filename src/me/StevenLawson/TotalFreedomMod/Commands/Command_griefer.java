457cadb162c441bd77aafbde677be1c60a23791d
Wilee999 <Infinitywilee@rocketmail.com>
2013-06-11 02:20:24 -0600

    Fix formatting (attempt 3)

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_griefer.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_griefer.java
@@ -15,18 +15,18 @@ public class Command_griefer extends TFM_Command
    @Override
    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
    {
-     if (args.length == 0)
-     {
-       TFM_Util.adminbcastMsg(sender.getName() + " is getting griefed!", ChatColor.RED);
-       return true;
-       }
+      if (args.length == 0)
+      {
+        TFM_Util.adminbcastMsg(sender.getName() + " is getting griefed!", ChatColor.RED);
+        return true;
+      }
 		
-       String message = StringUtils.join(args, " ");
+      String message = StringUtils.join(args, " ");
 		
-       TFM_Util.adminbcastMsg(String.format("[GrieferReport:%s] %s", sender.getName(), message), ChatColor.RED);
+      TFM_Util.adminbcastMsg(String.format("[GrieferReport:%s] %s", sender.getName(), message), ChatColor.RED);
 		
-       return true;
-        //On-request: Credit to mrnintendofan for the idea!
+      return true;
+      //On-request: Credit to mrnintendofan for the idea!
    }
 	
 }
