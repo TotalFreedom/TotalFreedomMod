b9bd0461057bad8623fcb7f40f97f99a8e57dfb8
Steven Lawson <steven@smlawson.com>
2013-07-11 20:35:09 -0400

    Changed WorldEdit API access method.

--- src/me/StevenLawson/TotalFreedomMod/TFM_WorldEditBridge.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_WorldEditBridge.java
@@ -2,8 +2,6 @@ package me.StevenLawson.TotalFreedomMod;
 
 import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.bukkit.BukkitPlayer;
-import com.sk89q.worldedit.bukkit.BukkitServerInterface;
-import com.sk89q.worldedit.bukkit.WorldEditAPI;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -12,8 +10,6 @@ import org.bukkit.plugin.Plugin;
 public class TFM_WorldEditBridge
 {
     private WorldEditPlugin worldEditPlugin = null;
-    private WorldEditAPI worldEditAPI = null;
-    private BukkitServerInterface bukkitServerInterface = null;
 
     private TFM_WorldEditBridge()
     {
@@ -23,57 +19,56 @@ public class TFM_WorldEditBridge
     {
         if (this.worldEditPlugin == null)
         {
-            Plugin we = Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
-            if (we != null)
+            try
             {
-                if (we instanceof WorldEditPlugin)
+                Plugin we = Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
+                if (we != null)
                 {
-                    this.worldEditPlugin = (WorldEditPlugin) we;
+                    if (we instanceof WorldEditPlugin)
+                    {
+                        this.worldEditPlugin = (WorldEditPlugin) we;
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                TFM_Log.severe(ex);
+            }
         }
-
         return this.worldEditPlugin;
-    }
-
-    public WorldEditAPI getWorldEditAPI()
-    {
-        if (this.worldEditAPI == null)
-        {
-            WorldEditPlugin wep = getWorldEditPlugin();
-            if (wep != null)
-            {
-                this.worldEditAPI = new WorldEditAPI(wep);
-            }
-        }
-
-        return this.worldEditAPI;
-    }
-
-    public BukkitServerInterface getBukkitServerInterface()
-    {
-        if (this.bukkitServerInterface == null)
-        {
-            WorldEditPlugin wep = this.getWorldEditPlugin();
-            if (wep != null)
-            {
-                this.bukkitServerInterface = new BukkitServerInterface(wep, Bukkit.getServer());
-            }
-        }
-
-        return this.bukkitServerInterface;
     }
 
     public BukkitPlayer getBukkitPlayer(Player p)
     {
-        WorldEditPlugin wep = this.getWorldEditPlugin();
-        BukkitServerInterface bsi = this.getBukkitServerInterface();
-
-        if (wep != null && bsi != null)
+        try
         {
-            return new BukkitPlayer(wep, bsi, p);
+            WorldEditPlugin wep = this.getWorldEditPlugin();
+            if (wep != null)
+            {
+                return wep.wrapPlayer(p);
+            }
         }
+        catch (Exception ex)
+        {
+            TFM_Log.severe(ex);
+        }
+        return null;
+    }
 
+    public LocalSession getPlayerSession(Player p)
+    {
+        try
+        {
+            WorldEditPlugin wep = this.getWorldEditPlugin();
+            if (wep != null)
+            {
+                return wep.getSession(p);
+            }
+        }
+        catch (Exception ex)
+        {
+            TFM_Log.severe(ex);
+        }
         return null;
     }
 
@@ -81,13 +76,12 @@ public class TFM_WorldEditBridge
     {
         try
         {
-            WorldEditAPI api = this.getWorldEditAPI();
-            if (api != null)
+            LocalSession session = getPlayerSession(p);
+            if (session != null)
             {
-                LocalSession session = api.getSession(p);
-                if (session != null)
+                BukkitPlayer bukkitPlayer = this.getBukkitPlayer(p);
+                if (bukkitPlayer != null)
                 {
-                    BukkitPlayer bukkitPlayer = this.getBukkitPlayer(p);
                     for (int i = 0; i < count; i++)
                     {
                         session.undo(session.getBlockBag(bukkitPlayer), bukkitPlayer);
@@ -105,14 +99,10 @@ public class TFM_WorldEditBridge
     {
         try
         {
-            WorldEditAPI api = this.getWorldEditAPI();
-            if (api != null)
+            LocalSession session = getPlayerSession(p);
+            if (session != null)
             {
-                LocalSession session = api.getSession(p);
-                if (session != null)
-                {
-                    session.setBlockChangeLimit(limit);
-                }
+                session.setBlockChangeLimit(limit);
             }
         }
         catch (Exception ex)
