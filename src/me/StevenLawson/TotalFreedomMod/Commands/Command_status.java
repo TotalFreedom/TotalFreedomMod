e56c273457aa4e7bb09235d6307c0f0afffb54ab
Jerome van der Sar <jerom.sar@hotmail.com>
2013-07-13 22:11:13 +0200

    Moved TotalFreedomMod-related information to /tfm

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_status.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_status.java
@@ -40,11 +40,9 @@ public class Command_status extends TFM_Command
     @Override
     public boolean run(final CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        playerMsg(String.format("Total Freedom Mod v%s.%s, built %s.", TotalFreedomMod.pluginVersion, TotalFreedomMod.buildNumber, TotalFreedomMod.buildDate), ChatColor.GOLD);
-        playerMsg("TotalFreedomMod was created by Madgeek1450 and DarthSalamon.", ChatColor.GOLD);
+        playerMsg("For information about TotalFreedomMod, try /tfm", ChatColor.GREEN); // Temporary
 
         playerMsg("Server is currently running with 'online-mode=" + (server.getOnlineMode() ? "true" : "false") + "'.", ChatColor.YELLOW);
-
         playerMsg("Loaded worlds:", ChatColor.BLUE);
         int i = 0;
         for (World world : server.getWorlds())
