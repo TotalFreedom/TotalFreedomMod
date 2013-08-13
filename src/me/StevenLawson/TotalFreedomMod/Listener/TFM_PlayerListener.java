656dbf88675b967ca35ec13ee6a8251dc8831f46
JeromSar <jerom.sar@hotmail.com>
2013-08-13 16:05:37 +0200

    Started work on Jumppads

--- src/me/StevenLawson/TotalFreedomMod/Listener/TFM_PlayerListener.java
+++ src/me/StevenLawson/TotalFreedomMod/Listener/TFM_PlayerListener.java
@@ -322,6 +322,11 @@ public class TFM_PlayerListener implements Listener
             }
         }
 
+        if (TFM_Jumppads.getInstance().mode.isOn())
+        {
+            TFM_Jumppads.getInstance().PlayerMoveEvent(event);
+        }
+
         if (TotalFreedomMod.landminesEnabled && TotalFreedomMod.allowExplosions)
         {
             Iterator<TFM_LandmineData> landmines = TFM_LandmineData.landmines.iterator();
@@ -359,6 +364,7 @@ public class TFM_PlayerListener implements Listener
                 }
             }
         }
+
     }
 
     @EventHandler(priority = EventPriority.NORMAL)
