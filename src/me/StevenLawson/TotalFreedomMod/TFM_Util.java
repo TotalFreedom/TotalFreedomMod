dcb6a4513ebd2f439653fe7574b264d5a04915e0
Ryan <ryan.ictclubsales@gmail.com>
2013-08-22 20:29:16 +0100

    Removed Disaster from Developer access
    Noticed this when working on the CJFreedom version, thought it should be removed!

--- src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
@@ -25,7 +25,7 @@ public class TFM_Util
     public static final Map<String, EntityType> mobtypes = new HashMap<String, EntityType>();
     public static final List<String> STOP_COMMANDS = Arrays.asList("stop", "off", "end", "halt", "die");
     public static final List<String> REMOVE_COMMANDS = Arrays.asList("del", "delete", "rem", "remove");
-    public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "DarthSalamon", "AcidicCyanide", "wild1145", "HeXeRei452", "disaster839");
+    public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "DarthSalamon", "AcidicCyanide", "wild1145", "HeXeRei452");
 
     static
     {
