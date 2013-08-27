0b146943ffb9b455e070f1c6dc0d6d341e940cf4
JeromSar <jerom.sar@hotmail.com>
2013-08-27 16:22:46 +0200

    Added /rollback undo

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_rollback.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_rollback.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH, block_host_console = true)
-@CommandParameters(description = "Issues a rollback on a player", usage = "/<command> <[partialname] | purge [partialname] | purgeall>", aliases = "rb")
+@CommandParameters(description = "Issues a rollback on a player", usage = "/<command> <[partialname] | undo [partialname] purge [partialname] | purgeall>", aliases = "rb")
 public class Command_rollback extends TFM_Command
 {
     @Override
@@ -16,24 +16,57 @@ public class Command_rollback extends TFM_Command
     {
         if (args.length == 1)
         {
-            if (args[0].equalsIgnoreCase("purgeall"))
+            if ("purgeall".equalsIgnoreCase(args[0]))
             {
-                TFM_Util.adminAction(sender.getName(), "Purging all rollback history.", false);
+                TFM_Util.adminAction(sender.getName(), "Purging all rollback history", false);
                 playerMsg("Purged all rollback history for " + TFM_RollbackManager.purgeEntries() + " players.");
             }
             else
             {
                 String playerName = getPlayerName(args[0]);
+
+                if (!TFM_RollbackManager.canRollback(playerName))
+                {
+                    playerMsg("That player has no entries stored.");
+                    return true;
+                }
+
+                if (TFM_RollbackManager.canUndoRollback(playerName))
+                {
+                    playerMsg("That player has just been rolled back.");
+                }
+
                 TFM_Util.adminAction(sender.getName(), "Rolling back player: " + playerName, false);
                 playerMsg("Rolled back " + TFM_RollbackManager.rollback(playerName) + " edits for " + playerName + ".");
+                playerMsg("If this rollback was a mistake, use /rollback undo " + playerName + " within 20 seconds to reverse the rollback.");
             }
         }
         else if (args.length == 2)
         {
-            if (args[0].equalsIgnoreCase("purge"))
+            if ("purge".equalsIgnoreCase(args[0]))
             {
                 String playerName = getPlayerName(args[1]);
+
+                if (!TFM_RollbackManager.canRollback(playerName))
+                {
+                    playerMsg("That player has no entries stored.");
+                    return true;
+                }
+
                 playerMsg("Purged " + TFM_RollbackManager.purgeEntries(playerName) + " rollback history entries for " + playerName + ".");
+            }
+            else if ("undo".equalsIgnoreCase(args[0]))
+            {
+                String playerName = getPlayerName(args[1]);
+
+                if (!TFM_RollbackManager.canUndoRollback(playerName))
+                {
+                    playerMsg("That player hasn't been rolled back recently.");
+                    return true;
+                }
+
+                TFM_Util.adminAction(sender.getName(), "Reverting rollback for player: " + playerName, false);
+                playerMsg("Reverted " + TFM_RollbackManager.undoRollback(playerName) + " edits for " + playerName + ".");
             }
             else
             {
