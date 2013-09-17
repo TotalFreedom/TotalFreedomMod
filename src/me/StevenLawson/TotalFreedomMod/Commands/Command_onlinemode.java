7b593508330eaa0cf8bbc7da6c8df88042a6604d
StevenLawson <steven@smlawson.com>
2013-09-17 12:05:53 -0400

    Tweak Wilee's changes to /onlinemode.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_onlinemode.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_onlinemode.java
@@ -2,13 +2,15 @@ package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Log;
 import me.StevenLawson.TotalFreedomMod.TFM_ServerInterface;
+import me.StevenLawson.TotalFreedomMod.TFM_SuperadminList;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.StevenLawson.TotalFreedomMod.TotalFreedomMod;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH, block_host_console = true)
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
 @CommandParameters(description = "Switch server online-mode on and off.", usage = "/<command> <on | off>")
 public class Command_onlinemode extends TFM_Command
 {
@@ -18,17 +20,18 @@ public class Command_onlinemode extends TFM_Command
         if (args.length < 1)
         {
             playerMsg("Server is currently running with 'online-mode=" + (server.getOnlineMode() ? "true" : "false") + "'.", ChatColor.WHITE);
-            playerMsg("If you have Telnet, you can use \"/onlinemode on\" and \"/onlinemode off\" to change online mode.", ChatColor.WHITE);
+            playerMsg("\"/onlinemode on\" and \"/onlinemode off\" can be used to change online mode from the console.", ChatColor.WHITE);
         }
         else
         {
             boolean online_mode;
-            
-            if (sender instanceof Player && !TFM_SuperadminList.isSeniorAdmin(sender))
+
+            if (sender instanceof Player && !TFM_SuperadminList.isSeniorAdmin(sender, true))
             {
                 playerMsg(TotalFreedomMod.MSG_NO_PERMS);
                 return true;
             }
+
             if (args[0].equalsIgnoreCase("on"))
             {
                 online_mode = true;
