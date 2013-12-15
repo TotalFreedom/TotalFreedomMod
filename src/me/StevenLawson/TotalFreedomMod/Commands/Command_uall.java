4e72c31ce8871f1f58f3fdcf9e6ea28079f5cf31
Wilee999 <Infinitywilee@rocketmail.com>
2013-12-15 15:33:26 -0800

    If disguisecraft isn't installed/enabled, return it to the sender. Fixes /uall.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_uall.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_uall.java
@@ -5,6 +5,7 @@ import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
 @CommandParameters(description = "Undisguises all players", usage = "/<command>")
@@ -13,9 +14,16 @@ public class Command_uall extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_Util.adminAction(sender.getName(), "Undisguising all players", true);
-
-        TFM_DisguiseCraftBridge.undisguiseAllPlayers();
+        Plugin disguiseCraft = server.getPluginManager().getPlugin("DisguiseCraft");
+        if (disguiseCraft != null)
+        {
+            TFM_Util.adminAction(sender.getName(), "Undisguising all players", true);
+            TFM_DisguiseCraftBridge.undisguiseAllPlayers();
+        }
+        else
+        {
+            playerMsg("I apologize, however DisguiseCraft is not enabled/installed on this server.", ChatColor.RED);
+        }
 
         return true;
     }
