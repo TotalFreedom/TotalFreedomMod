05884ae806ba45aa58e8b784da8925779a67db67
unknown <jerom.sar@hotmail.com>
2013-12-17 15:16:32 +0100

    Case-sensitive Telnet login names

--- src/me/StevenLawson/TotalFreedomMod/Listener/TFM_CustomListener.java
+++ src/me/StevenLawson/TotalFreedomMod/Listener/TFM_CustomListener.java
@@ -1,7 +1,10 @@
 package me.StevenLawson.TotalFreedomMod.Listener;
 
+import me.StevenLawson.TotalFreedomMod.TFM_Log;
 import me.StevenLawson.TotalFreedomMod.TFM_Superadmin;
 import me.StevenLawson.TotalFreedomMod.TFM_SuperadminList;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -10,16 +13,30 @@ public class TFM_CustomListener implements Listener
 {
     @EventHandler(priority = EventPriority.NORMAL)
     public void onTelnetPreLogin(me.StevenLawson.BukkitTelnet.TelnetPreLoginEvent event)
-    {
-        String ip = event.getIp();
-        if (ip != null && !ip.isEmpty())
+    {  
+        
+        final String ip = event.getIp();
+        if (ip == null || ip.isEmpty())
         {
-            TFM_Superadmin admin = TFM_SuperadminList.getAdminEntryByIP(ip, true);
-            if (admin != null && (admin.isTelnetAdmin() || admin.isSeniorAdmin()))
-            {
-                event.setBypassPassword(true);
-                event.setName(admin.getName());
-            }
+            return;
         }
+
+        final TFM_Superadmin admin = TFM_SuperadminList.getAdminEntryByIP(ip, true);
+
+        if (admin == null || !(admin.isTelnetAdmin() || admin.isSeniorAdmin()))
+        {
+            return;
+        }
+
+        event.setBypassPassword(true);
+        event.setName(admin.getName());
+
+        final OfflinePlayer player = Bukkit.getOfflinePlayer(admin.getName());
+        if (player == null)
+        {
+            return;
+        }
+        
+        event.setName(player.getName());
     }
 }
