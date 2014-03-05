ac6e86b4ffa3285f671fc2c15d58d6ff5aa7f973
Wilee999 <Infinitywilee@rocketmail.com>
2014-03-05 10:50:32 -0800

    I really need to start importing things.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_doom.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_doom.java
@@ -7,6 +7,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.util.Vector;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
