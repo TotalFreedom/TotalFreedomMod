9bc0859fdf6c219fbec50a517e0fcfe1048868c0
unknown <jerom.sar@hotmail.com>
2014-05-05 16:03:15 +0200

    Added /tagnyan. Resolves #170

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_tagnyan.java
@@ -0,0 +1,37 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import me.StevenLawson.TotalFreedomMod.TFM_PlayerData;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.OP, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Gives you a tag with random colors", usage = "/<command> <tag>", aliases = "tn")
+public class Command_tagnyan extends TFM_Command
+{
+    @Override
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length < 1)
+        {
+            return false;
+        }
+
+        final StringBuilder tag = new StringBuilder();
+
+        for (char c : ChatColor.stripColor(TFM_Util.colorize(StringUtils.join(args, " "))).toCharArray())
+        {
+            tag.append(TFM_Util.randomChatColor()).append(c);
+        }
+
+        final TFM_PlayerData data = TFM_PlayerData.getPlayerData(sender_p);
+        data.setTag(tag.toString());
+
+        playerMsg("Set tag to " + tag);
+
+        return true;
+    }
+}
