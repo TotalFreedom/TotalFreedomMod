c490a3d62ee82e14f7abd9680b3077635d78f6bf
Ryan <Admin@superior-networks.com>
2014-08-14 22:37:20 +0100

    Now made qdeop require you to be op else it wont try to deop the player.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_qdeop.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_qdeop.java
@@ -34,9 +34,12 @@ public class Command_qdeop extends TFM_Command
         {
             if (player.getName().toLowerCase().contains(targetName) || player.getDisplayName().toLowerCase().contains(targetName))
             {
-                matchedPlayerNames.add(player.getName());
-                player.setOp(false);
-                player.sendMessage(TotalFreedomMod.YOU_ARE_NOT_OP);
+                if (player.isOp())
+                {
+                    matchedPlayerNames.add(player.getName());
+                    player.setOp(false);
+                    player.sendMessage(TotalFreedomMod.YOU_ARE_NOT_OP);
+                }
             }
         }
 
