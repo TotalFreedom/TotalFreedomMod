9272665138eff8db3eb9aa1060e3e08100feeb94
Ryan <Admin@superior-networks.com>
2014-09-02 18:59:08 +0100

    Had to update TFM for 1.7.10 else I cant make the changes and test them. This is a separate commit to make it easy if you want to reverse this change.

--- src/me/StevenLawson/TotalFreedomMod/TFM_ServerInterface.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_ServerInterface.java
@@ -6,8 +6,8 @@ import java.util.List;
 import java.util.UUID;
 import java.util.regex.Pattern;
 import me.StevenLawson.TotalFreedomMod.Config.TFM_ConfigEntry;
-import net.minecraft.server.v1_7_R3.MinecraftServer;
-import net.minecraft.server.v1_7_R3.PropertyManager;
+import net.minecraft.server.v1_7_R4.MinecraftServer;
+import net.minecraft.server.v1_7_R4.PropertyManager;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
@@ -16,14 +16,14 @@ import org.bukkit.event.player.PlayerLoginEvent.Result;
 
 public class TFM_ServerInterface
 {
-    public static final String COMPILE_NMS_VERSION = "v1_7_R3";
+    public static final String COMPILE_NMS_VERSION = "v1_7_R4";
     public static final Pattern USERNAME_REGEX = Pattern.compile("^[\\w\\d_]{3,20}$");
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd \'at\' HH:mm:ss z");
 
     public static void setOnlineMode(boolean mode)
     {
         final PropertyManager manager = MinecraftServer.getServer().getPropertyManager();
-        manager.a("online-mode", mode);
+       // manager.a("online-mode", mode);
         manager.savePropertiesFile();
     }
 
