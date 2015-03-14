bd9cc94e8330f08d46cf6e6abafa6086a58c00f9
Robinson Gallego <robinson.leal7@gmail.com>
2015-03-13 23:15:51 -0400

    Update Command_tban.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_tban.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_tban.java
@@ -43,7 +43,8 @@ public class Command_tban extends TFM_Command
 
         TFM_Util.adminAction(sender.getName(), "Tempbanning: " + player.getName() + " for 5 minutes.", true);
         // tempbanning with ip
-        TFM_BanManager.addIpBan(new TFM_Ban(playerIp, player.getName(), sender.getName(), TFM_Util.parseDateOffset("5m"), ChatColor.RED + "You have been temporarily banned for 5 minutes."));
+        TFM_BanManager.addIpBan(
+                new TFM_Ban(playerIp, player.getName(), sender.getName(), TFM_Util.parseDateOffset("5m"), ChatColor.RED + "You have been temporarily banned for 5 minutes."));
         TFM_BanManager.addUuidBan(
                 new TFM_Ban(TFM_UuidManager.getUniqueId(player), player.getName(), sender.getName(), TFM_Util.parseDateOffset("5m"), ChatColor.RED + "You have been temporarily banned for 5 minutes."));
 
