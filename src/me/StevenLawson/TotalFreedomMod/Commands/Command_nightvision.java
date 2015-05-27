e36fc627ad32a7339602fab67a6e1fc54f6b5466
Will Swoveland <tkdguy1212@gmail.com>
2015-05-26 22:00:33 -0700

    Fixed indentation, formatting, etc...

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_nightvision.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_nightvision.java
@@ -1,28 +1,31 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
-import net.md_5.bungee.api.ChatColor;
-
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+/**
+ *
+ * @author Will
+ */
 @CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
 @CommandParameters(description = "Enables/Disables nightvision.", usage = "/<command>",  aliases = "nv")
 public class Command_nightvision extends TFM_Command {
 
-	@Override
+    @Override
 	public boolean run(CommandSender sender, Player sender_p, Command cmd,
 			String commandLabel, String[] args, boolean senderIsConsole) {
 		Player player = (Player) sender;
 		if (commandLabel.equalsIgnoreCase("nightvision")) {
 			if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
 				player.removePotionEffect(PotionEffectType.NIGHT_VISION);
-				player.sendMessage(ChatColor.RED + "NightVision Disabled!");
+				player.sendMessage(ChatColor.RED + "Night vision Disabled!");
 
 			} else {
-				player.sendMessage(ChatColor.AQUA + "Nightvision Enabled");
+				player.sendMessage(ChatColor.AQUA + "Night vision Enabled");
 				player.addPotionEffect(new PotionEffect(
 						PotionEffectType.NIGHT_VISION, 9999999, 1));
 				return false;
