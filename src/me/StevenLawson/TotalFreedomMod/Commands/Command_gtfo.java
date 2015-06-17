c3db6ef19d8a7a17e42393bcfb000020df69a8b9
Scuph <hentscheljr1@aol.com>
2015-06-17 12:31:36 -0400

    Update Command_gtfo.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_gtfo.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_gtfo.java
@@ -16,7 +16,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Makes someone GTFO (deop and ip ban by username).", usage = "/<command> <partialname><reason[optional]>")
+@CommandParameters(description = "Makes someone GTFO (deop and ip ban by username).", usage = "/<command> <partialname> <reason[optional]>")
 public class Command_gtfo extends TFM_Command
 {
     @Override
