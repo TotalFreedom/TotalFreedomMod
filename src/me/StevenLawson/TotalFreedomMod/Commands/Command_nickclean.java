41282fce9462609e46d86eb461971d6ec1f9a8f7
Adaptified <hentscheljr1@aol.com>
2015-08-10 03:08:05 -0400

    Solves issue
    Currently italics are allowed; and nickclean wipes any user's nickname if it contains such characters.
    See http://totalfreedom.boards.net/thread/26577/allow-italics.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_nickclean.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_nickclean.java
@@ -18,7 +18,6 @@ public class Command_nickclean extends TFM_Command
     {
         ChatColor.MAGIC,
         ChatColor.STRIKETHROUGH,
-        ChatColor.ITALIC,
         ChatColor.UNDERLINE,
         ChatColor.BLACK
     };
