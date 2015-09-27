fb9fdf3203dba215754acaa3c9de7440dff9d690
OutOfYourMind <outofyourmindyt@gmail.com>
2015-09-27 15:34:39 -0400

    Update TFM_PlayerRank.java

--- src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
@@ -10,6 +10,7 @@ public enum TFM_PlayerRank
 {
     DEVELOPER("a " + ChatColor.DARK_PURPLE + "Developer", ChatColor.DARK_PURPLE + "[Dev]"),
     EXEC("an " + ChatColor.YELLOW + "Executive", ChatColor.YELLOW + "[Executive]"),
+    IFDEV("an " + ChatColor.DARK_PURPLE + "Developer", ChatColor.DARK_PURPLE + "[IF-Dev]"),
     IMPOSTOR("an " + ChatColor.YELLOW + ChatColor.UNDERLINE + "Impostor", ChatColor.YELLOW.toString() + ChatColor.UNDERLINE + "[IMP]"),
     NON_OP("a " + ChatColor.GREEN + "Non-OP", ChatColor.GREEN.toString()),
     OP("an " + ChatColor.RED + "OP", ChatColor.RED + "[OP]"),
