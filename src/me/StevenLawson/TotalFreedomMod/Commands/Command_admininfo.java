7df102be6650ee8e888a4333176ed8a7272ef45e
aggelosQQ <jouflis1@hotmail.com>
2015-09-27 23:44:57 +0300

    Added some things in /ai

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
@@ -14,11 +14,14 @@ public class Command_admininfo extends TFM_Command
     {
         if (args.length == 0)
         {
-            sender.sendMessage(ChatColor.LIGHT_PURPLE + "You wish to apply for admin do you?");
-            sender.sendMessage(ChatColor.RED + "Well, first of all you need to register a forum account at http://immafreedom.site88.net/");
-            sender.sendMessage(ChatColor.AQUA + "Then, copy the template at http://immafreedom.site88.net/showthread.php?tid=3");
-            sender.sendMessage(ChatColor.GOLD + "After that, make a new thread by going to this link http://immafreedom.site88.net/newthread.php?fid=9");
-            sender.sendMessage(ChatColor.GREEN + "Then, paste the template, and answer all of the questions.");
+            sender.sendMessage(ChatColor.GOLD + "============HOW TO APPLY============");
+            sender.sendMessage(ChatColor.AQUA + "If you want to be an administrator, you need to follow these steps:");
+            sender.sendMessage(ChatColor.AQUA + "First of all you need to register a forum account at http://immafreedom.site88.net/");
+            sender.sendMessage(ChatColor.AQUA + "Secondly, copy the template at http://immafreedom.site88.net/showthread.php?tid=3");
+            sender.sendMessage(ChatColor.AQUA + "After that, make a new thread by going to this link http://immafreedom.site88.net/newthread.php?fid=9");
+            sender.sendMessage(ChatColor.AQUA + "Finally, paste the template, and answer all of the questions.");
+            sender.sendMessage(ChatColor.RED + "Please make sure you meet all the requirements before you submit your application!");
+            sender.sendMessage(ChatColor.GOLD + "============HOW TO APPLY============");
             return true;
         }
         else
