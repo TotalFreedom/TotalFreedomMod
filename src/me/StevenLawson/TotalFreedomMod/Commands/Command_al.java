2cd1281876d4ef455489d60124da3aa0f7cf2774
aggelosQQ <jouflis1@hotmail.com>
2015-09-29 11:21:46 +0300

    Changed the messages for /al

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_al.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_al.java
@@ -14,16 +14,11 @@ public class Command_al extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        sender.sendMessage(ChatColor.AQUA + "- Super Admins -");
-        sender.sendMessage(ChatColor.AQUA + StringUtils.join(TFM_AdminList.getSuperAdminNames(), ", "));
-        sender.sendMessage(ChatColor.DARK_GREEN + "- Telnet Admins -");
-        sender.sendMessage(ChatColor.DARK_GREEN + StringUtils.join(TFM_AdminList.getTelnetAdminNames(), ", "));
-        sender.sendMessage(ChatColor.LIGHT_PURPLE + "- Senior Admins -");
-        sender.sendMessage(ChatColor.LIGHT_PURPLE + StringUtils.join(TFM_AdminList.getSeniorAdminNames(), ", "));
+        sender.sendMessage(ChatColor.AQUA + "- Administrators without a rule can only be viewed by using '/saconfig list' -");
         sender.sendMessage(ChatColor.DARK_RED + "Executives: KM_Galahad, Exotic_Starlight");
-        sender.sendMessage(ChatColor.DARK_RED + "System Admins: None.");
+        // sender.sendMessage(ChatColor.DARK_RED + "System Admins: None");
         sender.sendMessage(ChatColor.DARK_PURPLE + "Developers: AwesomePinch");
-        sender.sendMessage(ChatColor.BLUE + "Owners: aggelosQQ");
+        sender.sendMessage(ChatColor.BLUE + "Owner: aggelosQQ");
 
         return true;
     }
