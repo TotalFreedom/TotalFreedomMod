b95af95acc68b971e1f628a20beecc13dfb42d31
aggelosQQ <jouflis1@hotmail.com>
2015-09-30 12:21:20 +0300

    Typos

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_al.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_al.java
@@ -14,7 +14,7 @@ public class Command_al extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        sender.sendMessage(ChatColor.AQUA + "- Administrators without a rule can only be viewed by using '/saconfig list' -");
+        sender.sendMessage(ChatColor.AQUA + "- Administrators without a role can only be viewed by using '/saconfig list' or on the forum! -");
         sender.sendMessage(ChatColor.DARK_RED + "Executives: KM_Galahad, Exotic_Starlight");
         // sender.sendMessage(ChatColor.DARK_RED + "System Admins: None");
         sender.sendMessage(ChatColor.DARK_PURPLE + "Developers: AwesomePinch");
