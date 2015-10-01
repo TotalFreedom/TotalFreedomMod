f6a2fcb1ae76492c8834870f8dd9af74dbcb927c
aggelosQQ <jouflis1@hotmail.com>
2015-10-01 21:49:03 +0300

    Updated /al

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_adminlist.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_adminlist.java
@@ -8,8 +8,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.OP, source = SourceType.BOTH)
-@CommandParameters(description = "Shows all admins", usage = "/<command>", aliases = "adminlist")
-public class Command_al extends TFM_Command
+@CommandParameters(description = "It shows senior administrators with a role!", usage = "/<command>", aliases = "al")
+public class Command_adminlist extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
@@ -17,7 +17,7 @@ public class Command_al extends TFM_Command
         sender.sendMessage(ChatColor.AQUA + "- Administrators without a role can only be viewed by using '/saconfig list' or on the forum! -");
         sender.sendMessage(ChatColor.DARK_RED + "Executives: KM_Galahad, Exotic_Starlight");
         // sender.sendMessage(ChatColor.DARK_RED + "System Admins: None");
-        sender.sendMessage(ChatColor.DARK_PURPLE + "Developers: AwesomePinch");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "Developers: AwesomePinch, AndySixx");
         sender.sendMessage(ChatColor.BLUE + "Owner: aggelosQQ");
 
         return true;
