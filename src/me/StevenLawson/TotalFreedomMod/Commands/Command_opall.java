07b8f623fdf975b09b67e9c42c536a99d26c78b1
LegendIsAwesomes <beanben@outlook.com>
2015-10-01 08:04:43 +0100

    Update Command_opall.java
    Fixed stuff.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_opall.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_opall.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Op everyone on the server, optionally change everyone's gamemode at the same time.", usage = "/<command> [-c | -s]")
+@CommandParameters(description = "Op everyone on the server, optionally change everyone's gamemode at the same time.", usage = "/<command> [-c | -s] [-a]")
 public class Command_opall extends TFM_Command
 {
     @Override
@@ -34,11 +34,7 @@ public class Command_opall extends TFM_Command
                 doSetGamemode = true;
                 targetGamemode = GameMode.ADVENTURE;
             }
-            else if (args[0].equals("-spectate"))
-            {
-                doSetGamemode = true;
-                targetGamemode = GameMode.SPECTATOR;
-            }
+        
         }
 
         for (Player player : server.getOnlinePlayers())
