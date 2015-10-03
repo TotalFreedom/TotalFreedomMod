09468bca0d47f020f102aa91e0c10d3e28404909
OutOfYourMind <outofyourmindyt@gmail.com>
2015-10-03 10:25:44 -0400

    Update Command_admininfo.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_admininfo.java
@@ -1,25 +1,32 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
-import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
-import org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.OP, source = SourceType.BOTH)
-@CommandParameters(description = "It shows senior administrators with a role!", usage = "/<command>", aliases = "al")
-public class Command_adminlist extends TFM_Command
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Apply for admin.", usage = "/<command>", aliases = "ai")
+public class Command_admininfo extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        sender.sendMessage(ChatColor.AQUA + "- Administrators without a role can only be viewed by using '/saconfig list' or on the forum! -");
-        sender.sendMessage(ChatColor.DARK_RED + "Executives: KM_Galahad, Exotic_Starlight");
-        // sender.sendMessage(ChatColor.DARK_RED + "System Admins: None");
-        sender.sendMessage(ChatColor.DARK_PURPLE + "Developers: AwesomePinch, AndySixx");
-        sender.sendMessage(ChatColor.BLUE + "Owner: aggelosQQ");
-         return true;
+        if (args.length == 0)
+        {
+            sender.sendMessage(ChatColor.GOLD + "============HOW TO APPLY============");
+            sender.sendMessage(ChatColor.AQUA + "If you want to be an administrator, you need to follow these steps:");
+            sender.sendMessage(ChatColor.AQUA + "First of all you need to register a forum account at http://immafreedom.site88.net/");
+            sender.sendMessage(ChatColor.AQUA + "Secondly, copy the template at http://immafreedom.site88.net/showthread.php?tid=3");
+            sender.sendMessage(ChatColor.AQUA + "After that, make a new thread by going to this link http://immafreedom.site88.net/newthread.php?fid=9");
+            sender.sendMessage(ChatColor.AQUA + "Finally, paste the template, and answer all of the questions.");
+            sender.sendMessage(ChatColor.RED + "Please make sure you meet all the requirements before you submit your application!");
+            sender.sendMessage(ChatColor.GOLD + "============HOW TO APPLY============");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
-} 
-    
+}
