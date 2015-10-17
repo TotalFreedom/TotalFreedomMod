172f08b8119acbf5b3cb38b7a126ed48b3b38632
PacksGamingHD <craftyswans@gmail.com>
2015-10-17 15:00:19 -0700

    testing more formatting

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_pptoggle.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_pptoggle.java
@@ -20,9 +20,9 @@ public class Command_pptoggle extends TFM_Command
     		    if (p.isEnabled()) {
                     p.getPluginLoader().disablePlugin(p);
     	            TFM_Util.adminAction(sender.getName(), "Disabling PlayerParticles", true);
-    			} else {
-    		        p.getPluginLoader().enablePlugin(p);
-    		        TFM_Util.adminAction(sender.getName(), "Enabled PlayerParticles", false);
+    	    } else {
+    		  p.getPluginLoader().enablePlugin(p);
+    		  TFM_Util.adminAction(sender.getName(), "Enabled PlayerParticles", false);
     			    }
     		    }
             }
