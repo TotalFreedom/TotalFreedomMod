1df4e8d4c6a077edfb44eae0949579fe9deb288e
Tyler Wrenn <mcbeat257@gmail.com>
2015-10-18 11:58:33 -0400

    Critical Bug Fixes

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_adminmsg.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_adminmsg.java
@@ -14,16 +14,20 @@ public class Command_adminmsg extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        TFM_AdminMessenger.adminMessengerMsg(sender, StringUtils.join(args, " "), senderIsConsole);
-        final TFM_PlayerData playerdata = TFM_PlayerData.getPlayerData(sender_p);
-        playerdata.setAdminHelpNeeded(true);
-
-        if (args.length == 2)
+        if (args.length == 0)
         {
+            return false;
+        }
+        if (args.length == 1)
+        {
+            final TFM_PlayerData playerdata = TFM_PlayerData.getPlayerData(sender_p);
             if (args[0].equalsIgnoreCase("off"))
             {
                 playerdata.setAdminHelpNeeded(false);
+                return true;
             }
+            TFM_AdminMessenger.adminMessengerMsg(sender, StringUtils.join(args, " "), senderIsConsole);
+            playerdata.setAdminHelpNeeded(true);
         }
         return true;
     }
