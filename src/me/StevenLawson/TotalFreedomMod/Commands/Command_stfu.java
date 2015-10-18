53b005eb5f5cf597ffd971dcb9eef09aa1db845f
LegendIsAwesomes <beanben@outlook.com>
2015-10-18 20:44:43 +0100

    Update Command_stfu.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_stfu.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_stfu.java
@@ -93,7 +93,7 @@ public class Command_stfu extends TFM_Command
             }
             else
             {
-                String reason;
+                String reason = "Unknown";
                 if (args.length >= 2)
                 {
                     reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
