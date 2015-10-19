501f7223737a385d9bedbf193a3e4700df13eb29
Lemon <minecraftoxlemonxo@gmail.com>
2015-10-19 22:32:54 +0500

    Fix.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_glist.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_glist.java
@@ -10,6 +10,7 @@ import me.StevenLawson.TotalFreedomMod.TFM_Player;
 import me.StevenLawson.TotalFreedomMod.TFM_PlayerList;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import me.StevenLawson.TotalFreedomMod.TFM_UuidManager;
+import me.StevenLaweson.TotalFreedomMod.TFM_RollbackManager;
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -98,7 +99,7 @@ public class Command_glist extends TFM_Command
             }
             else if (mode.equalsIgnoreCase("unban"))
             {
-                if (TFM_RollbackManager.canUndoRollback(username) 
+                if (TFM_RollbackManager.canUndoRollback(username))
                 {
                     TFM_RollbackManager.undoRollback(username);
                 }
