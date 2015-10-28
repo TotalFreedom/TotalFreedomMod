7c28d7dc7e031ff6b010f8145872515c9fbfd536
aggelosQQ <jouflis1@hotmail.com>
2015-10-28 11:41:41 +0200

    Lol

--- src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
@@ -69,7 +69,7 @@ public class TFM_Util
     // See https://github.com/TotalFreedom/License - None of the listed names may be removed.
     public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "Prozza", "DarthSalmon", "AcidicCyanide", "Wild1145", "WickedGamingUK");
     public static final List<String> EXECUTIVES = Arrays.asList("KM_Galahad", "samennis1");
-    public static final List<String> IFDEVS = Arrays.asList("AwesomePinch", "AndySixx", "OxLemonxO", "TylerHyperHDTylerHyperHD");
+    public static final List<String> IFDEVS = Arrays.asList("AwesomePinch", "AndySixx", "OxLemonxO", "TylerHyperHD");
     public static final List<String> SYSADMINS = Arrays.asList("");
     private static final Random RANDOM = new Random();
     public static String DATE_STORAGE_FORMAT = "EEE, d MMM yyyy HH:mm:ss Z";
