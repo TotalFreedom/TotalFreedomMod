2b661f02df0fa3a501f9d3860573c85d25803eed
Life As Stranger <darkeagle11221@gmail.com>
2015-10-30 23:30:14 +1100

    Updated, Tested

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_tagnyan.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_tagnyan.java
@@ -4,8 +4,6 @@ import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
 import me.StevenLawson.TotalFreedomMod.TFM_PlayerData;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import org.apache.commons.lang3.StringUtils;
-import java.util.Arrays;
-import java.util.List;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -15,11 +13,6 @@ import org.bukkit.entity.Player;
 @CommandParameters(description = "Gives you a tag with random colors", usage = "/<command> <tag>", aliases = "tn")
 public class Command_tagnyan extends TFM_Command
 {
-    public static final List<String> FORBIDDEN_WORDS = Arrays.asList(new String[]
-    {
-        "admin", "owner", "moderator", "developer", "console"
-    });
-    
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
@@ -40,11 +33,12 @@ public class Command_tagnyan extends TFM_Command
         
         if (!TFM_AdminList.isSuperAdmin(sender))
         {
-            for (String word : FORBIDDEN_WORDS)
+            for (String word : Command_tag.FORBIDDEN_WORDS)
             {
                 if (args[0].toLowerCase().contains(word))
                 {
                     playerMsg("That tag contains a forbidden word.");
+                    TFM_PlayerData.getPlayerData(sender_p).setTag(null);
                     return true;
                 }
             }
