cbf7435fa4adb281fce96ce2e479dd0612fe6b9b
Tyler Wrenn <mcbeat257@gmail.com>
2015-11-05 17:41:01 -0500

    Fixes #28

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_opall.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_opall.java
@@ -1,6 +1,8 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -13,6 +15,18 @@ public class Command_opall extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
+        int opedPlayers = 0;
+        for(Player player : Bukkit.getOnlinePlayers()) {
+            if (player.isOp()) {
+                opedPlayers++;
+            }
+        }
+        
+        if (opedPlayers == 0) {
+            sender.sendMessage(ChatColor.RED + "All players are opped. Operation aborted.");
+            return true;
+        }
+        
         TFM_Util.adminAction(sender.getName(), "Opping all players on the server", false);
 
         boolean doSetGamemode = false;
