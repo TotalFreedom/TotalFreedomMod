38db3f4c35af38be169afa0102825fc98c8273f7
LegendIsAwesomes <beanben@outlook.com>
2015-11-27 20:41:03 +0000

    Fixing Samennis1's rank
    Fixing Samennis1's rank

--- src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_Util.java
@@ -68,9 +68,9 @@ public class TFM_Util
     public static final Map<String, EntityType> mobtypes = new HashMap<String, EntityType>();
     // See https://github.com/TotalFreedom/License - None of the listed names may be removed.
     public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "Prozza", "DarthSalmon", "AcidicCyanide", "Wild1145", "WickedGamingUK");
-    public static final List<String> EXECUTIVES = Arrays.asList("KM_Galahad", "samennis1");
+    public static final List<String> EXECUTIVES = Arrays.asList("KM_Galahad");
     public static final List<String> IFDEVS = Arrays.asList("AwesomePinch", "AndySixx", "OxLemonxO", "tylerhyperHD");
-    public static final List<String> SYSADMINS = Arrays.asList("");
+    public static final List<String> SYSADMINS = Arrays.asList("samennis1");
     private static final Random RANDOM = new Random();
     public static String DATE_STORAGE_FORMAT = "EEE, d MMM yyyy HH:mm:ss Z";
     public static final Map<String, ChatColor> CHAT_COLOR_NAMES = new HashMap<String, ChatColor>();
