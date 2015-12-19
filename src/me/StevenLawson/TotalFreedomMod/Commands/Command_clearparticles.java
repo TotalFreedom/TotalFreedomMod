28d04ed722ddb013626faf50a85b2ce334b98b31
Adamx120 <wsfreehunter@hotmail.com>
2015-12-19 16:53:53 +0100

    Finally ClearParticles Command are Done.
    Finally it's done :D

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_clearparticles.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_clearparticles.java
@@ -8,16 +8,16 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Particle Cleanup", usage = "/<command>", aliases = "cc")
+@CommandParameters(description = "Particle Cleanup", usage = "/<command>", aliases = "cp")
 public class Command_clearparticles extends TFM_Command 
 {        
     @Override
-    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole) {
-        for (int i = 0; i <= 80; i++)
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
         {    
-            TFM_Util.bcastMsg("");
+        server.dispatchCommand(sender, "wildcard gcmd ? pp clear");
         }
-        TFM_Util.adminAction(sender.getName(), "Clearing all particles ", true);
+        TFM_Util.bcastMsg(sender.getName() + " - Cleared all particle effects from all players", ChatColor.RED);
         return true;
     }
 }
