cd881efe27f5d1bdd1de9bf6450e4dca5839e8c9
DUFCLiam <liam.townsley42@googlemail.com>
2015-12-19 18:35:32 +0000

    Update Command_smite.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_smite.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_smite.java
@@ -1,22 +1,24 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Someone being a little bitch? Smite them down...", usage = "/<command> [playername]")
+@CommandParameters(description = "Someone being a little bitch? Smite them down...", usage = "/<command> [playername] [reason]")
 public class Command_smite extends TFM_Command
 {
+    private static Object args;
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
-    {
-        if (args.length != 1)
+        {
+        if (args.length == 0)
         {
             return false;
         }
@@ -25,41 +27,55 @@ public class Command_smite extends TFM_Command
 
         if (player == null)
         {
-            playerMsg(TFM_Command.PLAYER_NOT_FOUND);
+            playerMsg(TFM_Command.PLAYER_NOT_FOUND, ChatColor.RED);
             return true;
         }
 
-        smite(player);
+        String reason = null;
+        if (args.length >= 2)
+        {
+            reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+        }
 
-        return true;
-    }
+        TFM_Util.bcastMsg(player.getName() + " has been a smitted for: ", ChatColor.RED);
 
-    public static void smite(final Player player)
-    {
-        TFM_Util.bcastMsg(player.getName() + " has been a naughty, naughty boy.", ChatColor.RED);
-
-        //Deop
+        // deop
         player.setOp(false);
 
-        //Set gamemode to survival:
+        // set gamemode to survival:
         player.setGameMode(GameMode.SURVIVAL);
 
-        //Clear inventory:
+        // clear inventory:
         player.getInventory().clear();
 
-        //Strike with lightning effect:
+        // strike with lightning effect:
         final Location targetPos = player.getLocation();
-        final World world = player.getWorld();
         for (int x = -1; x <= 1; x++)
         {
             for (int z = -1; z <= 1; z++)
             {
-                final Location strike_pos = new Location(world, targetPos.getBlockX() + x, targetPos.getBlockY(), targetPos.getBlockZ() + z);
-                world.strikeLightning(strike_pos);
+                final Location strike_pos = new Location(targetPos.getWorld(), targetPos.getBlockX() + x, targetPos.getBlockY(), targetPos.getBlockZ() + z);
+                targetPos.getWorld().strikeLightning(strike_pos);
             }
         }
 
-        //Kill:
+        //Kills:
         player.setHealth(0.0);
+        
+        final StringBuilder bcast = new StringBuilder()
+                .append(ChatColor.RED)
+                .append("Smiting: ")
+                
+                
+                
+
+        if (reason != null)
+        {
+            bcast.append(" - Reason: ").append(ChatColor.YELLOW).append(reason);
+        }
+
+        TFM_Util.bcastMsg(bcast.toString());
+
+        return true;
     }
 }
