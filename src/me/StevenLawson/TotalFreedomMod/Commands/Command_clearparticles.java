a0edd94a34dbe60294fe3c9a1fdfe97bf8f402b9
Adamx120 <wsfreehunter@hotmail.com>
2015-12-20 12:12:09 +0100

    Formatting /purgeparticles
    More Formatting

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_clearparticles.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_clearparticles.java
@@ -1,23 +1,23 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
-import me.StevenLawson.TotalFreedomMod.TFM_Util;
-import org.apache.commons.lang3.StringUtils;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Particle Cleanup", usage = "/<command>", aliases = "cp")
-public class Command_clearparticles extends TFM_Command 
-{        
+@CommandParameters(description = "Clearing all particle effect from all players on the server", usage = "/<command> <partialname>")
+public class Command_purgeparticles extends TFM_Command
+{
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
-    {
-        {    
-        server.dispatchCommand(sender, "wildcard gcmd ? pp clear");
-        }
-        TFM_Util.bcastMsg(sender.getName() + " - Cleared all particle effects from all players", ChatColor.RED);
+
+        {           
+            {
+                for (Player player : server.getOnlinePlayers())
+                {
+                server.dispatchCommand(player, "pp clear");
+                }
+            }
         return true;
     }
 }
