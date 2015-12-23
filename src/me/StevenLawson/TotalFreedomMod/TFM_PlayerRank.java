e9d8d3867ea1cbdee83b79dc0727e3d177c9e3c2
Savnith <MlHopper2020@gmail.com>
2015-12-22 22:18:07 -0800

    AlexFreedomMod: 6.1

--- src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
+++ src/me/StevenLawson/TotalFreedomMod/TFM_PlayerRank.java
@@ -2,20 +2,24 @@ package me.StevenLawson.TotalFreedomMod;
 
 import me.StevenLawson.TotalFreedomMod.Config.TFM_ConfigEntry;
 import static me.StevenLawson.TotalFreedomMod.TFM_Util.DEVELOPERS;
+import static me.StevenLawson.TotalFreedomMos.TFM_Util.SAVNITH;
+import static me.StevenLawson.TotalFreedomMod.TFM_Util.ADMIN;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public enum TFM_PlayerRank
 {
-    DEVELOPER("a " + ChatColor.DARK_PURPLE + "Developer", ChatColor.DARK_PURPLE + "[Dev]"),
+    DEVELOPER("a " + ChatColor.DARK_PURPLE + "Developer", ChatColor.DARK_PURPLE + "[Developer]"),
+    SAVNITH("the " + ChatColor.DARK_PURPLE + "Lead-Developer", ChatColor.DARK_PURPLE + "[L-Developer(Sav)]"),
     IMPOSTOR("an " + ChatColor.YELLOW + ChatColor.UNDERLINE + "Impostor", ChatColor.YELLOW.toString() + ChatColor.UNDERLINE + "[IMP]"),
     NON_OP("a " + ChatColor.GREEN + "Non-OP", ChatColor.GREEN.toString()),
     OP("an " + ChatColor.RED + "OP", ChatColor.RED + "[OP]"),
     SUPER("a " + ChatColor.GOLD + "Super Admin", ChatColor.GOLD + "[SA]"),
     TELNET("a " + ChatColor.DARK_GREEN + "Super Telnet Admin", ChatColor.DARK_GREEN + "[STA]"),
     SENIOR("a " + ChatColor.LIGHT_PURPLE + "Senior Admin", ChatColor.LIGHT_PURPLE + "[SrA]"),
-    OWNER("the " + ChatColor.BLUE + "Owner", ChatColor.BLUE + "[Owner]"),
+    ADMIN("a " + ChatColor.RED + "Administrator", ChatColor.DARK_RED + "[Administration]"),
+    OWNER("the " + ChatColor.RED + "Founder", ChatColor.BLUE + "[Founder]"),
     CONSOLE("The " + ChatColor.DARK_PURPLE + "Console", ChatColor.DARK_PURPLE + "[Console]");
     private final String loginMessage;
     private final String prefix;
@@ -68,6 +72,16 @@ public enum TFM_PlayerRank
         if (DEVELOPERS.contains(sender.getName()))
         {
             return DEVELOPER;
+        }
+      
+        if (ADMIN.contains(sender.getName())
+        {
+            return ADMIN;
+        {
+
+        if (SAVNITH.contains(sender.getName())
+        {
+            return SAVNITH;
         }
 
         final TFM_Admin entry = TFM_AdminList.getEntryByIp(TFM_Util.getIp((Player) sender));
