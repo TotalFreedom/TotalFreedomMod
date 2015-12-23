1c7f1ee78aa10c69c3b824b44356f6463fd9d908
Savnith <MlHopper2020@gmail.com>
2015-12-22 21:55:14 -0800

    Update Command_opall.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_opall.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_opall.java
@@ -14,7 +14,7 @@ public class Command_opall extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        Bukkit.broadcastMessage(ChatColor.BLUE + sender.getName() + " - Opping all players on the server.");
+        Bukkit.broadcastMessage(ChatColor.BLUE + sender.getName() + " - Opping all players on AlexFreedom.");
 
         boolean doSetGamemode = false;
         GameMode targetGamemode = GameMode.CREATIVE;
