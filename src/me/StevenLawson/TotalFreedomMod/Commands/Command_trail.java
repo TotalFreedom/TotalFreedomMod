dac2d358c99ac697c16fd7efcce06e03bb339c51
Adamx120 <wsfreehunter@hotmail.com>
2015-12-25 09:44:27 +0100

    Command_trail.java
    Added glass function to the trail command.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_trail.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_trail.java
@@ -18,7 +18,7 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.RegisteredListener;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.ONLY_IN_GAME)
-@CommandParameters(description = "Pretty rainbow trails.", usage = "/<command> [off]")
+@CommandParameters(description = "Pretty rainbow trails.", usage = "/<command> [off] [glass]")
 public class Command_trail extends TFM_Command
 {
     private static Listener movementListener = null;
@@ -28,6 +28,32 @@ public class Command_trail extends TFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
+        if (args.length > 0 && "glass".equals(args[0]))
+        {
+            Bukkit.getPluginManager().registerEvents(movementListener = new Listener()
+            {
+                @EventHandler(priority = EventPriority.NORMAL)
+                public void onPlayerMove(PlayerMoveEvent event)
+                {
+                    Player player = event.getPlayer();
+                    if (trailPlayers.contains(player))
+                    {
+                        Block fromBlock = event.getFrom().getBlock();
+                        if (fromBlock.isEmpty())
+                        {
+                            Block toBlock = event.getTo().getBlock();
+                            if (!fromBlock.equals(toBlock))
+                            {  
+                                
+                                fromBlock.setType(Material.STAINED_GLASS);
+                                TFM_DepreciationAggregator.setData_Block(fromBlock, (byte) RANDOM.nextInt(16));
+                            }
+                        }
+                    }
+                }
+            }, TotalFreedomMod.plugin);
+        }
+    
         if (args.length > 0 && "off".equals(args[0]))
         {
             trailPlayers.remove(sender_p);
@@ -55,8 +81,8 @@ public class Command_trail extends TFM_Command
 
         return true;
     }
-
-    private static void registerMovementHandler()
+    
+    public static void registerMovementHandler()
     {
         if (getRegisteredListener(movementListener) == null)
         {
@@ -73,7 +99,8 @@ public class Command_trail extends TFM_Command
                         {
                             Block toBlock = event.getTo().getBlock();
                             if (!fromBlock.equals(toBlock))
-                            {
+                            {  
+                                
                                 fromBlock.setType(Material.WOOL);
                                 TFM_DepreciationAggregator.setData_Block(fromBlock, (byte) RANDOM.nextInt(16));
                             }
