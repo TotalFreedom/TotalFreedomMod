aa4c576731f943f8be36d23327cc972dea51b5e5
DUFCLiam <liam.townsley42@googlemail.com>
2015-12-30 21:15:20 +0000

    Update /setspawnworld
    Add this [-b] as this will broadcast the "[SERVER] DUFC_Liam: New spawn!"

--- src/main/java/me/totalfreedom/totalfreedommod/commands/Command_setspawnworld.java
+++ src/main/java/me/totalfreedom/totalfreedommod/commands/Command_setspawnworld.java
@@ -9,7 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = PlayerRank.SUPER_ADMIN, source = SourceType.ONLY_IN_GAME)
-@CommandParameters(description = "Set world spawnpoint.", usage = "/<command>")
+@CommandParameters(description = "Set world spawnpoint.", usage = "/<command> [-b]")
 public class Command_setspawnworld extends FreedomCommand
 {
 
@@ -20,12 +20,21 @@ public class Command_setspawnworld extends FreedomCommand
         playerSender.getWorld().setSpawnLocation(pos.getBlockX(), pos.getBlockY(), pos.getBlockZ());
 
         playerMsg("Spawn location for this world set to: " + FUtil.formatLocation(playerSender.getWorld().getSpawnLocation()));
-
+ 
         if (ConfigEntry.PROTECTAREA_ENABLED.getBoolean() && ConfigEntry.PROTECTAREA_SPAWNPOINTS.getBoolean())
         {
             plugin.pa.addProtectedArea("spawn_" + playerSender.getWorld().getName(), pos, ConfigEntry.PROTECTAREA_RADIUS.getDouble());
         }
-
+        if (args.length = 0)
+        {
+            return true;
+        }
+        else
+        {
+            if (args[0].equals("-b"))
+            {
+                TFM_Util.bcastMsg(String.format("[Server:%s] %s", sender.getName(), " New spawn!"), ChatColor.LIGHT_PURPLE);
+            }
         return true;
     }
 }
