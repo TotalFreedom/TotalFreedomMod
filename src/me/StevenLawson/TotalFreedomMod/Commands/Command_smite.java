dd7c4d394280dade6162ca9ddab9e6e9e2734ad6
Bladezo <offbladzo@gmail.com>
2016-01-08 11:19:46 +1030

    Added Smite Reason
    I made a smite reason so that a player knows what they did wrong before the admin does /say

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_smite.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_smite.java
@@ -1,6 +1,8 @@
 package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
@@ -10,13 +12,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Someone being a little bitch? Smite them down...", usage = "/<command> [playername]")
+@CommandParameters(description = "Someone being a little bitch? Smite them down...", usage = "/<command> [playername] [reason]")
 public class Command_smite extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        if (args.length != 1)
+        if (args.length < 1)
         {
             return false;
         }
@@ -28,15 +30,53 @@ public class Command_smite extends TFM_Command
             playerMsg(TFM_Command.PLAYER_NOT_FOUND);
             return true;
         }
+        else if (args.length > 1)
+        {
+            final String reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+            smite(sender, player, reason);
+            return true;
+        }
 
-        smite(player);
+        else
+        {
+            sender.sendMessage(ChatColor.RED + "You must supply a reason in order to smite someone.");
+            return true;
+        }
+    }
 
-        return true;
+    public static void smite(final CommandSender sender, final Player player, final String reason)
+    {
+        TFM_Util.bcastMsg(String.format("%s has been a naughty, naughty person.\nThey have  been smitten!\n" + ChatColor.GOLD + "Reason: %s (%s)", player.getName(), reason, sender.getName()), ChatColor.RED);
+        String full = String.format(ChatColor.RED + "%s has been smitten for %s", player.getName(), reason + " (" + sender.getName() + ")");
+
+        //Deop
+        player.setOp(false);
+
+        //Set gamemode to survival:
+        player.setGameMode(GameMode.SURVIVAL);
+
+        //Clear inventory:
+        player.getInventory().clear();
+
+        //Strike with lightning effect:
+        final Location targetPos = player.getLocation();
+        final World world = player.getWorld();
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int z = -1; z <= 1; z++)
+            {
+                final Location strike_pos = new Location(world, targetPos.getBlockX() + x, targetPos.getBlockY(), targetPos.getBlockZ() + z);
+                world.strikeLightning(strike_pos);
+            }
+        }
+
+        //Kill:
+        player.setHealth(0.0);
     }
 
     public static void smite(final Player player)
     {
-        TFM_Util.bcastMsg(player.getName() + " has been a naughty, naughty boy.", ChatColor.RED);
+        TFM_Util.bcastMsg(player.getName() + " has been a naughty, naughty boy.\nThey have  been smitten!", ChatColor.RED);
 
         //Deop
         player.setOp(false);
@@ -63,3 +103,4 @@ public class Command_smite extends TFM_Command
         player.setHealth(0.0);
     }
 }
+Status API Training Shop Blog About Pricing
