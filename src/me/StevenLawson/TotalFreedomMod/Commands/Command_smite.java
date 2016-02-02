45b25c21d245814b2862c034155d5a2646f49566
Scoop <hentscheljr1@aol.com>
2016-02-02 16:49:25 -0500

    Used method recursion - voided duplicate code.

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_smite.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_smite.java
@@ -12,7 +12,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Someone being a little bitch? Smite them down...", usage = "/<command> [playername] [reason]")
+@CommandParameters(description = "Someone being a little bitch? Smite them down...", usage = "/<command> [playername] [reason:optional]")
 public class Command_smite extends TFM_Command
 {
 
@@ -33,22 +33,30 @@ public class Command_smite extends TFM_Command
         }
         else if (args.length > 1)
         {
-            final String reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
+            String reason = StringUtils.join(ArrayUtils.subarray(args, 1, args.length), " ");
             smite(player, reason);
             return true;
         }
-        else
-        {
+        
+        else {
             smite(player);
         }
+
 
         return true;
     }
 
+        
+
     public static void smite(final Player player, final String reason)
     {
-        TFM_Util.bcastMsg(String.format("%s has been a naughty, naughty boy. \n" + ChatColor.YELLOW + "Reason: %s", player.getName(), reason), ChatColor.RED);
-
+        if (reason == null) {
+        TFM_Util.bcastMsg(player.getName() + " has been naughty, naughty boy.", ChatColor.RED);
+        }
+        
+        else {
+        TFM_Util.bcastMsg(player.getName() + " has been a naughty, naughty boy.\n" + ChatColor.YELLOW + "Reason: " + reason, ChatColor.RED);
+        }
         //Deop
         player.setOp(false);
 
@@ -69,37 +77,19 @@ public class Command_smite extends TFM_Command
                 world.strikeLightning(strike_pos);
             }
         }
+      
 
         //Kill:
         player.setHealth(0.0);
-    }
+        
+        
+}
 
+   // Creates the null reason, this is done because of method recursion, so no reason can be given and smite(player); will run, keeping other commands using smite in tact.
+    static String reason = null;
     public static void smite(final Player player)
-    {
-        TFM_Util.bcastMsg(player.getName() + " has been a naughty, naughty boy.", ChatColor.RED);
-
-        //Deop
-        player.setOp(false);
-
-        //Set gamemode to survival:
-        player.setGameMode(GameMode.SURVIVAL);
-
-        //Clear inventory:
-        player.getInventory().clear();
-
-        //Strike with lightning effect:
-        final Location targetPos = player.getLocation();
-        final World world = player.getWorld();
-        for (int x = -1; x <= 1; x++)
-        {
-            for (int z = -1; z <= 1; z++)
-            {
-                final Location strike_pos = new Location(world, targetPos.getBlockX() + x, targetPos.getBlockY(), targetPos.getBlockZ() + z);
-                world.strikeLightning(strike_pos);
-            }
-        }
-
-        //Kill:
-        player.setHealth(0.0);
+    {     
+        Command_smite.smite(player, reason);
     }
+
 }
