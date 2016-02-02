e6f2a65f9bfa52df258a6c0ff0bd2bb51f32886a
rovertdude <tsannoyingorange@gmail.com>
2016-02-02 17:18:53 -0600

    Update Command_plugincontrol.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_plugincontrol.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_plugincontrol.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
-@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
+@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
 @CommandParameters(description = "Manage plugins", usage = "/<command> <<enable | disable | reload> <pluginname>> | list>", aliases = "plc")
 public class Command_plugincontrol extends TFM_Command
 {
