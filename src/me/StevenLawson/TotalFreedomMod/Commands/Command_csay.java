acecb1d825ba983f2fcf8e2193572a975a609b28
rovertdude <tsannoyingorange@gmail.com>
2016-02-02 17:26:46 -0600

    Make /csay senior only

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_csay.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_csay.java
@@ -6,16 +6,16 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.ONLY_CONSOLE)
-@CommandParameters(description = "Telnet command - Send a chat message with chat formatting over telnet.", usage = "/<command> <message...>")
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH)
+@CommandParameters(description = "Senior command - Send a chat message with chat formatting.", usage = "/<command> <message...>")
 public class Command_csay extends TFM_Command
 {
     @Override
-    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsPlayer)
     {
         if (args.length > 0)
         {
-            TFM_Util.bcastMsg(String.format("§7[CONSOLE]§f<§c%s§f> %s", sender.getName(), StringUtils.join(args, " ")));
+            TFM_Util.bcastMsg(String.format("§7[Senior Broadcast]§f<§c%s§f> %s", sender.getName(), StringUtils.join(args, " ")));
         }
         return true;
     }
