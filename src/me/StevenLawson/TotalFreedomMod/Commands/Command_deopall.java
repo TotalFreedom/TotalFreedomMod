64dc932585147ec9aaa0d6b4707236d1f4f929a1
rovertdude <tsannoyingorange@gmail.com>
2016-02-04 15:18:54 -0600

    Update Command_deopall.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_deopall.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_deopall.java
@@ -5,7 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH, blockHostConsole = true)
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH, blockHostConsole = false)
 @CommandParameters(description = "Deop everyone on the server.", usage = "/<command>")
 public class Command_deopall extends TFM_Command
 {
