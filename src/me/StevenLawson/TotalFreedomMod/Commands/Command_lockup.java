53a2217c9107126201aac6ac7fc7bb77963eb095
rovertdude <tsannoyingorange@gmail.com>
2016-02-05 16:45:49 -0600

    Update Command_lockup.java
    Fixed #7

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_lockup.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_lockup.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
-@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.ONLY_CONSOLE, blockHostConsole = true)
+@CommandPermissions(level = AdminLevel.SENIOR, source = SourceType.BOTH, blockHostConsole = false)
 @CommandParameters(description = "Block target's minecraft input. This is evil, and I never should have wrote it.", usage = "/<command> <all | purge | <<partialname> on | off>>")
 public class Command_lockup extends TFM_Command
 {
