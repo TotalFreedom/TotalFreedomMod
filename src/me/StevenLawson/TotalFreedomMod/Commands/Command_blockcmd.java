2f4a6435f0bbeb0c406584998f5b5c109fcb707c
LegendIsAwesomes <beanben@outlook.com>
2016-02-07 21:07:01 +0000

    Update Command_blockcmd.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_blockcmd.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_blockcmd.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Block all commands for a specific player.", usage = "/<command> [purge] | [partialname] [-a]>", aliases = "blockcommands,blockcommand")
+@CommandParameters(description = "Block all commands for a specific player.", usage = "/<command> <-a | purge | partialname>", aliases = "blockcommands,blockcommand")
 public class Command_blockcmd extends TFM_Command
 {
     @Override
