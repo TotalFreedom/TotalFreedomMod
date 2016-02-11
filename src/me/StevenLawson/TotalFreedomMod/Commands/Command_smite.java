6c233aa2a6cf7c5cd2d67201a115daa78778da56
Scoop <hentscheljr1@aol.com>
2016-02-11 11:18:57 -0500

    Fix params

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_smite.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_smite.java
@@ -12,7 +12,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Someone being a little bitch? Smite them down...", usage = "/<command> [playername] [reason:optional]")
+@CommandParameters(description = "Someone being a little bitch? Smite them down...", usage = "/<command> <playername> [reason]")
 public class Command_smite extends TFM_Command
 {
 
