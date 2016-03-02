adde4ae8275cc7e3147ce0bf3753983c017e793c
Liam Townsley <DUFCLiam@users.noreply.github.com>
2016-03-02 12:50:58 +0000

    Update Command_announce.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_announce.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_announce.java
@@ -9,17 +9,19 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.SUPER, source = SourceType.BOTH)
-@CommandParameters(description = "Announce a message", usage = "/<command>", aliases = "announcement, announcer")
+@CommandParameters(description = "Announce a message to the server as the broadcaster.", usage = "/<command>", aliases = "announcement, announcer")
 public class Command_announce extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-    //
-     public static void announce(final Player player)
-     {
+    
+      public static void announce(final Player player)
+      {
         String message = StringUtils.join(ArrayUtils.subarray(args, 0, args.length), " ");
         TFM_Util.bcastMsg(TFM_ConfigEntry.ANNOUNCER_PREFIX.getString().replaceAll("&", "§") + message + " (" + sender.getName() + ")");
         return true;
+      }
+         
     }
 }
