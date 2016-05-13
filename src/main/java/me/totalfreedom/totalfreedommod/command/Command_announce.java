68a885e515579588df21ef62c549674b296c395c
OutOfYourMind <outofyourmindyt@gmail.com>
2016-05-13 18:52:12 -0400

    Update Command_announce.java

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_announce.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_announce.java
@@ -6,7 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH)
+@CommandPermissions(level = Rank.SENIOR_ADMIN, source = SourceType.BOTH)
 @CommandParameters(description = "Make an announcement", usage = "/<command> <message>")
 public class Command_announce extends FreedomCommand
 {
