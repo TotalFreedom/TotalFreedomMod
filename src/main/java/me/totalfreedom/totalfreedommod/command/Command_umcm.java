e6ea0605fa15d94e8e01d1e71251bc64518a18b1
CreeperSeth <creepersetht2@gmail.com>
2016-05-15 14:30:35 -0600

    Create Command_umcm.java

---
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_umcm.java
@@ -0,0 +1,52 @@
+package me.totalfreedom.totalfreedommod.command;
+
+import me.totalfreedom.totalfreedommod.TotalFreedomMod;
+import me.totalfreedom.totalfreedommod.config.ConfigEntry;
+import me.totalfreedom.totalfreedommod.config.MainConfig;
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import me.totalfreedom.totalfreedommod.util.FLog;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = Rank.NON_OP, source = SourceType.BOTH)
+@CommandParameters(description = "Shows information about UnraveledMCMod or reloads it.", usage = "/<command> [reload]")
+public class Command_umcm extends FreedomCommand
+{
+
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 1)
+        {
+            if (!args[0].equals("reload"))
+            {
+                return false;
+            }
+
+            if (!plugin.al.isAdmin(sender))
+            {
+                noPerms();
+                return true;
+            }
+
+            MainConfig.load();
+            plugin.services.stop();
+            plugin.services.start();
+
+            final String message = String.format("%s v%s reloaded.",
+                    TotalFreedomMod.pluginName,
+                    ConfigEntry.PLUGIN_VERSION.getString());
+
+            msg(message);
+            FLog.info(message);
+            return true;
+        }
+        msg("UnraveledMCMod for 'UnraveledMC', an all-op server.", ChatColor.GOLD);
+        msg("Running on " + ConfigEntry.SERVER_NAME.getString() + ".", ChatColor.GOLD);
+        msg("Created by aggelosqq, and PinchTheFinch", ChatColor.GOLD);
+
+        return true;
+    }
+}
