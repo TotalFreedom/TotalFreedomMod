75138040b71cbe3fca2dc4a7ab237eeb43d53da1
CreeperSeth <creepersetht2@gmail.com>
2016-05-17 21:15:56 -0600

    Block /wildcard stop

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_wildcard.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_wildcard.java
@@ -1,11 +1,17 @@
 package me.totalfreedom.totalfreedommod.command;
 
 import me.totalfreedom.totalfreedommod.rank.Rank;
+import me.totalfreedom.totalfreedommod.util.FUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import me.totalfreedom.totalfreedommod.admin.Admin;
+import me.totalfreedom.totalfreedommod.player.FPlayer;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
 
 @CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH, blockHostConsole = true)
 @CommandParameters(description = "Run any command on all users, username placeholder = ?.", usage = "/<command> [fluff] ? [fluff] ?")
@@ -38,6 +44,26 @@ public class Command_wildcard extends FreedomCommand
         if (args[0].equals("saconfig"))
         {
             msg("WOA, WTF are you trying to do???", ChatColor.RED);
+            return true;
+        }
+        if (args[0].equals("stop"))
+        {
+            msg("No, hell no, that is rouge activity right there, this has been logged!", ChatColor.RED);
+            if (!senderIsConsole)
+            {
+                Admin admin = plugin.al.getAdmin(playerSender);
+                admin.setActive(false);
+                plugin.al.save();
+                plugin.al.updateTables();
+                playerSender.setOp(false);
+                FPlayer playerData = plugin.pl.getPlayer(playerSender);
+                Location targetPos = playerSender.getLocation().clone().add(0, 1, 0);
+                playerData.getCageData().cage(targetPos, Material.GLASS, Material.AIR);
+                playerSender.setGameMode(GameMode.SURVIVAL);
+                playerSender.closeInventory();
+            }
+            FUtil.adminAction(sender.getName(), "Has just attempted to execute the command /wildcard stop", true);
+            FUtil.bcastMsg("This is rouge activity, " + (senderIsConsole ? sender.getName() + " is console! Please standby and alert an executive admin or owner!" : playerSender.getName() + " has been removed from the admin list, deopped, and caged!"), ChatColor.RED);
             return true;
         }
 
