05a7d0bd9c040181422a22fd8544f781d604f87e
CreeperSeth <creepersetht2@gmail.com>
2016-05-20 14:16:40 -0600

    Allow in-game toggle of self verification system

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_selfverify.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_selfverify.java
@@ -7,7 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-@CommandPermissions(level = Rank.SENIOR_CONSOLE, source = SourceType.ONLY_CONSOLE)
+@CommandPermissions(level = Rank.SENIOR_ADMIN, source = SourceType.BOTH)
 @CommandParameters(description = "Enable or disable self verification", usage = "/<command> [on | off]")
 public class Command_selfverify extends FreedomCommand
 {
