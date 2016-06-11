2ee2690e8b78333b92f77ff405ed021d98f2468b
CreeperSeth <creeperseth@mail.com>
2016-06-11 14:11:51 -0700

    Remove /tfm reload

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_totalfreedommod.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_totalfreedommod.java
@@ -2,9 +2,7 @@ package me.totalfreedom.totalfreedommod.command;
 
 import me.totalfreedom.totalfreedommod.TotalFreedomMod;
 import me.totalfreedom.totalfreedommod.config.ConfigEntry;
-import me.totalfreedom.totalfreedommod.config.MainConfig;
 import me.totalfreedom.totalfreedommod.rank.Rank;
-import me.totalfreedom.totalfreedommod.util.FLog;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -14,39 +12,13 @@ import org.bukkit.entity.Player;
  * See https://github.com/TotalFreedom/License - This file may not be edited or removed.
  */
 @CommandPermissions(level = Rank.NON_OP, source = SourceType.BOTH)
-@CommandParameters(description = "Shows information about TotalFreedomMod or reloads it", usage = "/<command> [reload]", aliases = "tfm")
+@CommandParameters(description = "Shows information about TotalFreedomMod", usage = "/<command>", aliases = "tfm")
 public class Command_totalfreedommod extends FreedomCommand
 {
 
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        if (args.length == 1)
-        {
-            if (!args[0].equals("reload"))
-            {
-                return false;
-            }
-
-            if (!plugin.al.isAdmin(sender))
-            {
-                noPerms();
-                return true;
-            }
-
-            plugin.config.load();
-            plugin.services.stop();
-            plugin.services.start();
-
-            final String message = String.format("%s v%s reloaded.",
-                    TotalFreedomMod.pluginName,
-                    TotalFreedomMod.pluginVersion);
-
-            msg(message);
-            FLog.info(message);
-            return true;
-        }
-
         TotalFreedomMod.BuildProperties build = TotalFreedomMod.build;
         msg("TotalFreedomMod for 'Total Freedom', the original all-op server.", ChatColor.GOLD);
         msg("Running on " + ConfigEntry.SERVER_NAME.getString() + ".", ChatColor.GOLD);
