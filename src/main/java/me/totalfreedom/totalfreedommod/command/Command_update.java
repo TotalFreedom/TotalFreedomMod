d45d1323c0cf6f0cd7e0fcacb0a55611b3108d23
CreeperSeth <creeperseth@mail.com>
2016-06-12 08:58:15 -0700

    Fix command syntex

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_update.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_update.java
@@ -11,7 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 @CommandPermissions(level = Rank.SENIOR_ADMIN, source = SourceType.ONLY_CONSOLE)
-@CommandParameters(description = "Update UnraveledMCMod", usage = "/<command> [URL]")
+@CommandParameters(description = "Update UnraveledMCMod", usage = "/<command> <URL>")
 public class Command_update extends FreedomCommand
 {
     @Override
