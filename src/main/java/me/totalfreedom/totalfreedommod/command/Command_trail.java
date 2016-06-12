05b21f83dd7bd2bed3019cefc6aede11515a4a92
CreeperSeth <creeperseth@mail.com>
2016-06-12 08:58:34 -0700

    Remove unused imports

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_trail.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_trail.java
@@ -1,22 +1,9 @@
 package me.totalfreedom.totalfreedommod.command;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import me.totalfreedom.totalfreedommod.TotalFreedomMod;
 import me.totalfreedom.totalfreedommod.rank.Rank;
-import me.totalfreedom.totalfreedommod.util.DepreciationAggregator;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.plugin.RegisteredListener;
 
 @CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.ONLY_IN_GAME)
 @CommandParameters(description = "Pretty rainbow trails.", usage = "/<command> [off]")
