bacc9cc45184b2184443d78b93afc9e48698cb22
TomCreeper <tompaszylk@gmail.com>
2016-06-15 16:56:49 +0100

    Update Rank.java

--- src/main/java/me/totalfreedom/totalfreedommod/rank/Rank.java
+++ src/main/java/me/totalfreedom/totalfreedommod/rank/Rank.java
@@ -11,7 +11,7 @@ public enum Rank implements Displayable
     OP("an", "Op", Type.PLAYER, "OP", ChatColor.RED),
     SUPER_ADMIN("a", "Super Admin", Type.ADMIN, "SA", ChatColor.AQUA),
     TELNET_ADMIN("a", "Telnet Admin", Type.ADMIN, "STA", ChatColor.DARK_GREEN),
-    SENIOR_ADMIN("a", "Senior Admin", Type.ADMIN, "SrA", ChatColor.GOLD),
+    SENIOR_ADMIN("a", "Senior Admin", Type.ADMIN, "SrA", ChatColor.LIGHT_PURPLE),
     TELNET_CONSOLE("the", "Console", Type.ADMIN_CONSOLE, "Console", ChatColor.DARK_PURPLE),
     SENIOR_CONSOLE("the", "Console", Type.ADMIN_CONSOLE, "Console", ChatColor.DARK_PURPLE);
     @Getter
