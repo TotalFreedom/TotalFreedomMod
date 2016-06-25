ce66e25ad5d690062de0c339cbab53df0eae4c90
Lemon <Lemon@Lemons>
2016-06-25 21:29:32 +0500

    Use CoreProtect in rollbacking.

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_rollback.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_rollback.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH, blockHostConsole = true)
-@CommandParameters(description = "Issues a rollback on a player", usage = "/<command> <[partialname] | undo [partialname] purge [partialname] | purgeall>", aliases = "rb")
+@CommandParameters(description = "Issues a rollback on a player", usage = "/<command> <[name] | undo [name]>", aliases = "rb")
 public class Command_rollback extends FreedomCommand
 {
 
@@ -21,65 +21,23 @@ public class Command_rollback extends FreedomCommand
 
         if (args.length == 1)
         {
-            if ("purgeall".equals(args[0]))
-            {
-                FUtil.adminAction(sender.getName(), "Purging all rollback history", false);
-                msg("Purged all rollback history for " + plugin.rb.purgeEntries() + " players.");
-            }
-            else
-            {
-                final String playerName = plugin.rb.findPlayer(args[0]);
-
-                if (playerName == null)
-                {
-                    msg("That player has no entries stored.");
-                    return true;
-                }
-
-                if (plugin.rb.canUndoRollback(playerName))
-                {
-                    msg("That player has just been rolled back.");
-                }
-
-                FUtil.adminAction(sender.getName(), "Rolling back player: " + playerName, false);
-                msg("Rolled back " + plugin.rb.rollback(playerName) + " edits for " + playerName + ".");
-                msg("If this rollback was a mistake, use /rollback undo " + playerName + " within 40 seconds to reverse the rollback.");
-            }
+            final String playerName = args[0];
+            FUtil.adminAction(sender.getName(), "Rolling back player: " + playerName, false);
+            server.dispatchCommand(sender, "co rb u:" + playerName + " t:10d r:#global");
+            msg("If this rollback was a mistake, use /rollback undo " + playerName + " to reverse the rollback.");
             return true;
         }
 
         if (args.length == 2)
         {
-            if ("purge".equalsIgnoreCase(args[0]))
-            {
-                final String playerName = plugin.rb.findPlayer(args[1]);
-
-                if (playerName == null)
-                {
-                    msg("That player has no entries stored.");
-                    return true;
-                }
-
-                msg("Purged " + plugin.rb.purgeEntries(playerName) + " rollback history entries for " + playerName + ".");
-                return true;
-            }
-
             if ("undo".equalsIgnoreCase(args[0]))
             {
-                final String playerName = plugin.rb.findPlayer(args[1]);
-
-                if (playerName == null)
-                {
-                    msg("That player hasn't been rolled back recently.");
-                    return true;
-                }
-
+                final String playerName = args[0];
                 FUtil.adminAction(sender.getName(), "Reverting rollback for player: " + playerName, false);
-                msg("Reverted " + plugin.rb.undoRollback(playerName) + " edits for " + playerName + ".");
+                server.dispatchCommand(sender, "co restore u:" + playerName + " t:10d r:#global");
                 return true;
             }
         }
-
         return false;
     }
 }
