925ba6b1195f1a069c57ca9440d718aae1420a52
CreeperSeth <creeperseth@mail.com>
2016-07-23 11:43:35 -0700

    Added my cracked alt "Ruby_Rose" as a developer

--- src/main/java/me/totalfreedom/totalfreedommod/util/FUtil.java
+++ src/main/java/me/totalfreedom/totalfreedommod/util/FUtil.java
@@ -38,8 +38,9 @@ public class FUtil
     // See https://github.com/TotalFreedom/License - None of the listed names may be removed.
     // Leave the DEVELOPERS List alone, those are the TotalFreedom Developers.
     // If a new DEVELOPER is added, list them under the UMCDEVS List.
+    // Ruby_Rose is CreeperSeth's cracked alt account
     public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "Prozza", "Wild1145", "WickedGamingUK");
-    public static final List<String> UMCDEVS = Arrays.asList("CreeperSeth", "OxLemonxO");
+    public static final List<String> UMCDEVS = Arrays.asList("CreeperSeth", "Ruby_Rose", "OxLemonxO");
     public static String DATE_STORAGE_FORMAT = "EEE, d MMM yyyy HH:mm:ss Z";
     public static final Map<String, ChatColor> CHAT_COLOR_NAMES = new HashMap<>();
     public static final List<ChatColor> CHAT_COLOR_POOL = Arrays.asList(
