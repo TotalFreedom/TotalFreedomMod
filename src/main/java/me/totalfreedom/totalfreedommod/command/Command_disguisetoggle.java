a3e6ca4425034d19aa1f8eeda846632b5a907af0
Lemon <minecraftoxlemonxo@gmail.com>
2016-07-31 10:41:30 +0500

    Remove unused import

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_disguisetoggle.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_disguisetoggle.java
@@ -7,7 +7,6 @@ import org.bukkit.command.Command;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginManager;
 
 @CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH)
 @CommandParameters(description = "Toggle the disguise plugin", usage = "/<command>", aliases = "dtoggle")
