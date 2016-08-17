5fc2edf71b983ec9edff7bcbbda909594f06ce31
Lemon <minecraftoxlemonxo@gmail.com>
2016-08-17 22:10:52 +0500

    Fixes

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_disguisetoggle.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_disguisetoggle.java
@@ -16,9 +16,13 @@ public class Command_disguisetoggle extends FreedomCommand
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
+        if(plugin.ldb.getLibsDisguisesPlugin() == null) {
+           msg(ChatColor.RED + "LibsDisguises is not enabled.");
+           return true;
+        }
 
         FUtil.adminAction(sender.getName(), (DisallowedDisguises.disabled ? "Enabling" : "Disabling") + " Disguises", false);
-        sender.sendMessage(ChatColor.DARK_GRAY + "Disguises" + (DisallowedDisguises.disabled ? "enabled." : "disabled."));
+        msg(ChatColor.DARK_GRAY + "Disguises" + (DisallowedDisguises.disabled ? "enabled." : "disabled."));
         
         if (!DisallowedDisguises.disabled)
         {
