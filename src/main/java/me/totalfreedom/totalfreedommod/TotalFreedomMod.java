e8fb9b6e78a560d507cfd18a47f50b10a46d8cbd
marcocorriero <marcocorriero@live.it>
2016-08-26 21:57:10 +0200

    Added Uall and Dtoggle

--- src/main/java/me/totalfreedom/totalfreedommod/TotalFreedomMod.java
+++ src/main/java/me/totalfreedom/totalfreedommod/TotalFreedomMod.java
@@ -1,58 +1,37 @@
 package me.totalfreedom.totalfreedommod;
 
-import me.totalfreedom.totalfreedommod.fun.Trailer;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-import me.totalfreedom.totalfreedommod.admin.AdminList;
-import me.totalfreedom.totalfreedommod.banning.BanManager;
-import me.totalfreedom.totalfreedommod.banning.PermbanList;
-import me.totalfreedom.totalfreedommod.blocking.BlockBlocker;
-import me.totalfreedom.totalfreedommod.blocking.EventBlocker;
-import me.totalfreedom.totalfreedommod.blocking.InteractBlocker;
-import me.totalfreedom.totalfreedommod.blocking.MobBlocker;
-import me.totalfreedom.totalfreedommod.blocking.PotionBlocker;
-import me.totalfreedom.totalfreedommod.blocking.command.CommandBlocker;
-import me.totalfreedom.totalfreedommod.bridge.BukkitTelnetBridge;
-import me.totalfreedom.totalfreedommod.bridge.EssentialsBridge;
-import me.totalfreedom.totalfreedommod.bridge.WorldEditBridge;
-import me.totalfreedom.totalfreedommod.caging.Cager;
-import me.totalfreedom.totalfreedommod.command.CommandLoader;
-import me.totalfreedom.totalfreedommod.config.MainConfig;
-import me.totalfreedom.totalfreedommod.freeze.Freezer;
-import me.totalfreedom.totalfreedommod.fun.ItemFun;
-import me.totalfreedom.totalfreedommod.fun.Jumppads;
-import me.totalfreedom.totalfreedommod.fun.Landminer;
-import me.totalfreedom.totalfreedommod.fun.MP44;
-import me.totalfreedom.totalfreedommod.httpd.HTTPDaemon;
-import me.totalfreedom.totalfreedommod.player.PlayerList;
-import me.totalfreedom.totalfreedommod.rank.RankManager;
-import me.totalfreedom.totalfreedommod.rollback.RollbackManager;
-import me.totalfreedom.totalfreedommod.util.FLog;
-import me.totalfreedom.totalfreedommod.util.FUtil;
-import me.totalfreedom.totalfreedommod.util.MethodTimer;
-import me.totalfreedom.totalfreedommod.world.WorldManager;
-import net.pravian.aero.component.service.ServiceManager;
-import net.pravian.aero.plugin.AeroPlugin;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.mcstats.Metrics;
+import net.pravian.aero.plugin.*;
+import me.totalfreedom.totalfreedommod.config.*;
+import net.pravian.aero.component.service.*;
+import me.totalfreedom.totalfreedommod.world.*;
+import me.totalfreedom.totalfreedommod.admin.*;
+import me.totalfreedom.totalfreedommod.rank.*;
+import me.totalfreedom.totalfreedommod.command.*;
+import me.totalfreedom.totalfreedommod.blocking.command.*;
+import me.totalfreedom.totalfreedommod.blocking.*;
+import me.totalfreedom.totalfreedommod.player.*;
+import me.totalfreedom.totalfreedommod.banning.*;
+import me.totalfreedom.totalfreedommod.rollback.*;
+import me.totalfreedom.totalfreedommod.caging.*;
+import me.totalfreedom.totalfreedommod.freeze.*;
+import me.totalfreedom.totalfreedommod.fun.*;
+import me.totalfreedom.totalfreedommod.httpd.*;
+import me.totalfreedom.totalfreedommod.bridge.*;
+import me.totalfreedom.totalfreedommod.util.*;
+import org.mcstats.*;
+import org.bukkit.plugin.*;
+import org.bukkit.scheduler.*;
+import org.bukkit.*;
+import java.util.*;
+import java.io.*;
 
 public class TotalFreedomMod extends AeroPlugin<TotalFreedomMod>
 {
-
     public static final String CONFIG_FILENAME = "config.yml";
-    //
-    public static final BuildProperties build = new BuildProperties();
-    //
+    public static final BuildProperties build;
     public static String pluginName;
     public static String pluginVersion;
-    //
     public MainConfig config;
-    //
-    // Services
     public ServiceManager<TotalFreedomMod> services;
     public ServerInterface si;
     public SavedFlags sf;
@@ -96,202 +75,152 @@ public class TotalFreedomMod extends AeroPlugin<TotalFreedomMod>
     public Jumppads jp;
     public Trailer tr;
     public HTTPDaemon hd;
-    //
-    // Bridges
     public ServiceManager<TotalFreedomMod> bridges;
     public BukkitTelnetBridge btb;
     public EssentialsBridge esb;
+    public LibsDisguisesBridge ldb;
     public WorldEditBridge web;
-
-    @Override
-    public void load()
-    {
-        TotalFreedomMod.pluginName = plugin.getDescription().getName();
-        TotalFreedomMod.pluginVersion = plugin.getDescription().getVersion();
-
-        FLog.setPluginLogger(plugin.getLogger());
-        FLog.setServerLogger(server.getLogger());
-
-        build.load(plugin);
+    
+    public void load() {
+        TotalFreedomMod.pluginName = ((TotalFreedomMod)this.plugin).getDescription().getName();
+        TotalFreedomMod.pluginVersion = ((TotalFreedomMod)this.plugin).getDescription().getVersion();
+        FLog.setPluginLogger(((TotalFreedomMod)this.plugin).getLogger());
+        FLog.setServerLogger(this.server.getLogger());
+        TotalFreedomMod.build.load((TotalFreedomMod)this.plugin);
     }
-
-    @Override
-    public void enable()
-    {
+    
+    public void enable() {
         FLog.info("Created by Madgeek1450 and Prozza");
-        FLog.info("Version " + build.formattedVersion());
-        FLog.info("Compiled " + build.date + " by " + build.author);
-
+        FLog.info("Version " + TotalFreedomMod.build.formattedVersion());
+        FLog.info("Compiled " + TotalFreedomMod.build.date + " by " + TotalFreedomMod.build.author);
         final MethodTimer timer = new MethodTimer();
         timer.start();
-
-        // Warn if we're running on a wrong version
         ServerInterface.warnVersion();
-
-        // Delete unused files
         FUtil.deleteCoreDumps();
         FUtil.deleteFolder(new File("./_deleteme"));
-
-        // Convert old config files
-        new ConfigConverter(plugin).convert();
-
-        BackupManager backups = new BackupManager(this);
-        backups.createBackups(TotalFreedomMod.CONFIG_FILENAME, true);
-        backups.createBackups(AdminList.CONFIG_FILENAME);
-        backups.createBackups(PermbanList.CONFIG_FILENAME);
-
-        config = new MainConfig(this);
-        config.load();
-
-        // Start services
-        services = new ServiceManager<>(plugin);
-        si = services.registerService(ServerInterface.class);
-        sf = services.registerService(SavedFlags.class);
-        wm = services.registerService(WorldManager.class);
-        lv = services.registerService(LogViewer.class);
-        al = services.registerService(AdminList.class);
-        rm = services.registerService(RankManager.class);
-        cl = services.registerService(CommandLoader.class);
-        cb = services.registerService(CommandBlocker.class);
-        eb = services.registerService(EventBlocker.class);
-        bb = services.registerService(BlockBlocker.class);
-        mb = services.registerService(MobBlocker.class);
-        ib = services.registerService(InteractBlocker.class);
-        pb = services.registerService(PotionBlocker.class);
-        lp = services.registerService(LoginProcess.class);
-        nu = services.registerService(AntiNuke.class);
-        as = services.registerService(AntiSpam.class);
-
-        pl = services.registerService(PlayerList.class);
-        an = services.registerService(Announcer.class);
-        cm = services.registerService(ChatManager.class);
-        bm = services.registerService(BanManager.class);
-        pm = services.registerService(PermbanList.class);
-        pa = services.registerService(ProtectArea.class);
-        gr = services.registerService(GameRuleHandler.class);
-
-        // Single admin utils
-        rb = services.registerService(RollbackManager.class);
-        cs = services.registerService(CommandSpy.class);
-        ca = services.registerService(Cager.class);
-        fm = services.registerService(Freezer.class);
-        or = services.registerService(Orbiter.class);
-        mu = services.registerService(Muter.class);
-        fo = services.registerService(Fuckoff.class);
-        ak = services.registerService(AutoKick.class);
-        ae = services.registerService(AutoEject.class);
-
-        mv = services.registerService(MovementValidator.class);
-        ew = services.registerService(EntityWiper.class);
-        fd = services.registerService(FrontDoor.class);
-        sp = services.registerService(ServerPing.class);
-
-        // Fun
-        it = services.registerService(ItemFun.class);
-        lm = services.registerService(Landminer.class);
-        mp = services.registerService(MP44.class);
-        jp = services.registerService(Jumppads.class);
-        tr = services.registerService(Trailer.class);
-
-        // HTTPD
-        hd = services.registerService(HTTPDaemon.class);
-        services.start();
-
-        // Start bridges
-        bridges = new ServiceManager<>(plugin);
-        btb = bridges.registerService(BukkitTelnetBridge.class);
-        esb = bridges.registerService(EssentialsBridge.class);
-        web = bridges.registerService(WorldEditBridge.class);
-        bridges.start();
-
+        new ConfigConverter((TotalFreedomMod)this.plugin).convert();
+        final BackupManager backups = new BackupManager(this);
+        backups.createBackups("config.yml", true);
+        backups.createBackups("admins.yml");
+        backups.createBackups("permbans.yml");
+        (this.config = new MainConfig(this)).load();
+        this.services = (ServiceManager<TotalFreedomMod>)new ServiceManager(this.plugin);
+        this.si = (ServerInterface)this.services.registerService((Class)ServerInterface.class);
+        this.sf = (SavedFlags)this.services.registerService((Class)SavedFlags.class);
+        this.wm = (WorldManager)this.services.registerService((Class)WorldManager.class);
+        this.lv = (LogViewer)this.services.registerService((Class)LogViewer.class);
+        this.al = (AdminList)this.services.registerService((Class)AdminList.class);
+        this.rm = (RankManager)this.services.registerService((Class)RankManager.class);
+        this.cl = (CommandLoader)this.services.registerService((Class)CommandLoader.class);
+        this.cb = (CommandBlocker)this.services.registerService((Class)CommandBlocker.class);
+        this.eb = (EventBlocker)this.services.registerService((Class)EventBlocker.class);
+        this.bb = (BlockBlocker)this.services.registerService((Class)BlockBlocker.class);
+        this.mb = (MobBlocker)this.services.registerService((Class)MobBlocker.class);
+        this.ib = (InteractBlocker)this.services.registerService((Class)InteractBlocker.class);
+        this.pb = (PotionBlocker)this.services.registerService((Class)PotionBlocker.class);
+        this.lp = (LoginProcess)this.services.registerService((Class)LoginProcess.class);
+        this.nu = (AntiNuke)this.services.registerService((Class)AntiNuke.class);
+        this.as = (AntiSpam)this.services.registerService((Class)AntiSpam.class);
+        this.pl = (PlayerList)this.services.registerService((Class)PlayerList.class);
+        this.an = (Announcer)this.services.registerService((Class)Announcer.class);
+        this.cm = (ChatManager)this.services.registerService((Class)ChatManager.class);
+        this.bm = (BanManager)this.services.registerService((Class)BanManager.class);
+        this.pm = (PermbanList)this.services.registerService((Class)PermbanList.class);
+        this.pa = (ProtectArea)this.services.registerService((Class)ProtectArea.class);
+        this.gr = (GameRuleHandler)this.services.registerService((Class)GameRuleHandler.class);
+        this.rb = (RollbackManager)this.services.registerService((Class)RollbackManager.class);
+        this.cs = (CommandSpy)this.services.registerService((Class)CommandSpy.class);
+        this.ca = (Cager)this.services.registerService((Class)Cager.class);
+        this.fm = (Freezer)this.services.registerService((Class)Freezer.class);
+        this.or = (Orbiter)this.services.registerService((Class)Orbiter.class);
+        this.mu = (Muter)this.services.registerService((Class)Muter.class);
+        this.fo = (Fuckoff)this.services.registerService((Class)Fuckoff.class);
+        this.ak = (AutoKick)this.services.registerService((Class)AutoKick.class);
+        this.ae = (AutoEject)this.services.registerService((Class)AutoEject.class);
+        this.mv = (MovementValidator)this.services.registerService((Class)MovementValidator.class);
+        this.ew = (EntityWiper)this.services.registerService((Class)EntityWiper.class);
+        this.fd = (FrontDoor)this.services.registerService((Class)FrontDoor.class);
+        this.sp = (ServerPing)this.services.registerService((Class)ServerPing.class);
+        this.it = (ItemFun)this.services.registerService((Class)ItemFun.class);
+        this.lm = (Landminer)this.services.registerService((Class)Landminer.class);
+        this.mp = (MP44)this.services.registerService((Class)MP44.class);
+        this.jp = (Jumppads)this.services.registerService((Class)Jumppads.class);
+        this.tr = (Trailer)this.services.registerService((Class)Trailer.class);
+        this.hd = (HTTPDaemon)this.services.registerService((Class)HTTPDaemon.class);
+        this.services.start();
+        this.bridges = (ServiceManager<TotalFreedomMod>)new ServiceManager(this.plugin);
+        this.btb = (BukkitTelnetBridge)this.bridges.registerService((Class)BukkitTelnetBridge.class);
+        this.esb = (EssentialsBridge)this.bridges.registerService((Class)EssentialsBridge.class);
+		this.ldb = (LibsDisguisesBridge)this.bridges.registerService((Class)LibsDisguisesBridge.class);
+        this.web = (WorldEditBridge)this.bridges.registerService((Class)WorldEditBridge.class);
+        this.bridges.start();
         timer.update();
-        FLog.info("Version " + pluginVersion + " for " + ServerInterface.COMPILE_NMS_VERSION + " enabled in " + timer.getTotal() + "ms");
-
-        // Metrics @ http://mcstats.org/plugin/TotalFreedomMod
-        try
-        {
-            final Metrics metrics = new Metrics(plugin);
+        FLog.info("Version " + TotalFreedomMod.pluginVersion + " for " + "v1_10_R1" + " enabled in " + timer.getTotal() + "ms");
+        try {
+            final Metrics metrics = new Metrics((Plugin)this.plugin);
             metrics.start();
         }
-        catch (IOException ex)
-        {
+        catch (IOException ex) {
             FLog.warning("Failed to submit metrics data: " + ex.getMessage());
         }
-
-        // Add spawnpoints later - https://github.com/TotalFreedom/TotalFreedomMod/issues/438
-        new BukkitRunnable()
-        {
-            @Override
-            public void run()
-            {
-                plugin.pa.autoAddSpawnpoints();
+        new BukkitRunnable() {
+            public void run() {
+                ((TotalFreedomMod)TotalFreedomMod.this.plugin).pa.autoAddSpawnpoints();
             }
-        }.runTaskLater(plugin, 60L);
+        }.runTaskLater((Plugin)this.plugin, 60L);
     }
-
-    @Override
-    public void disable()
-    {
-        // Stop services and bridges
-        bridges.stop();
-        services.stop();
-
-        server.getScheduler().cancelTasks(plugin);
-
+    
+    public void disable() {
+        this.bridges.stop();
+        this.services.stop();
+        this.server.getScheduler().cancelTasks((Plugin)this.plugin);
         FLog.info("Plugin disabled");
     }
-
+    
+    public static TotalFreedomMod plugin() {
+        for (final Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
+            if (plugin.getName().equalsIgnoreCase(TotalFreedomMod.pluginName)) {
+                return (TotalFreedomMod)plugin;
+            }
+        }
+        return null;
+    }
+    
+    static {
+        build = new BuildProperties();
+    }
+    
     public static class BuildProperties
     {
-
         public String author;
         public String codename;
         public String version;
         public String number;
         public String date;
         public String head;
-
-        public void load(TotalFreedomMod plugin)
-        {
-            try
-            {
-                final Properties props;
-                try (InputStream in = plugin.getResource("build.properties"))
-                {
+        
+        public void load(final TotalFreedomMod plugin) {
+            try {
+                Properties props;
+                try (final InputStream in = plugin.getResource("build.properties")) {
                     props = new Properties();
                     props.load(in);
                 }
-
-                author = props.getProperty("program.build.author", "unknown");
-                codename = props.getProperty("program.build.codename", "unknown");
-                version = props.getProperty("program.build.version", "unknown");
-                number = props.getProperty("program.build.number", "1");
-                date = props.getProperty("program.build.date", "unknown");
-                head = props.getProperty("program.build.head", "unknown");
+                this.author = props.getProperty("program.build.author", "unknown");
+                this.codename = props.getProperty("program.build.codename", "unknown");
+                this.version = props.getProperty("program.build.version", "unknown");
+                this.number = props.getProperty("program.build.number", "1");
+                this.date = props.getProperty("program.build.date", "unknown");
+                this.head = props.getProperty("program.build.head", "unknown");
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 FLog.severe("Could not load build properties! Did you compile with Netbeans/ANT?");
                 FLog.severe(ex);
             }
         }
-
-        public String formattedVersion()
-        {
-            return pluginVersion + "." + number + " (" + head + ")";
+        
+        public String formattedVersion() {
+            return TotalFreedomMod.pluginVersion + "." + this.number + " (" + this.head + ")";
         }
     }
-
-    public static TotalFreedomMod plugin()
-    {
-        for (Plugin plugin : Bukkit.getPluginManager().getPlugins())
-        {
-            if (plugin.getName().equalsIgnoreCase(pluginName))
-            {
-                return (TotalFreedomMod) plugin;
-            }
-        }
-        return null;
-    }
-
 }
