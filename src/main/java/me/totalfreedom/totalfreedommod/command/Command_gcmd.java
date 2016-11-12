493d672e3c0b2944f58fc1a5ff9534f3cbaa5c74
Commodore64x <Commodore64x@users.noreply.github.com>
2016-11-13 09:48:19 +1000

    Update Command_gcmd.java

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_gcmd.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_gcmd.java
@@ -1,10 +1,7 @@
 package me.totalfreedom.totalfreedommod.command;
 
-import java.util.Arrays;
-import java.util.List;
 import me.totalfreedom.totalfreedommod.rank.Rank;
 import org.apache.commons.lang3.StringUtils;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -14,72 +11,46 @@ import org.bukkit.entity.Player;
 public class Command_gcmd extends FreedomCommand
 {
 
-    public static final List<String> BLOCKED_COMMANDS = Arrays.asList(
-            "wildcard",
-            "gtfo",
-            "doom",
-            "saconfig",
-            "gcmd",
-            "glist",
-            "tban",
-            "tempban"
-    );
-
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
+        if (args.length < 2)
+        {
+            return false;
+        }
+
+        final Player player = getPlayer(args[0]);
+
+        if (player == null)
+        {
+            sender.sendMessage(FreedomCommand.PLAYER_NOT_FOUND);
+            return true;
+        }
+
+        final String outCommand = StringUtils.join(args, " ", 1, args.length);
+
+        if (plugin.cb.isCommandBlocked(outCommand, sender))
+        {
+            return true;
+        }
+
         try
         {
-            final Player player = getPlayer(args[0]);
-
-            if (args.length < 2)
+            msg("Sending command as " + player.getName() + ": " + outCommand);
+            if (server.dispatchCommand(player, outCommand))
             {
-                return false;
+                msg("Command sent.");
             }
-
-            if (player == null)
+            else
             {
-                sender.sendMessage(FreedomCommand.PLAYER_NOT_FOUND);
-                return true;
-            }
-
-            String outCommand = StringUtils.join(args, " ", 1, args.length);
-            Command runCmd = server.getPluginCommand(args[1]);
-            
-            if (BLOCKED_COMMANDS.contains(runCmd.getName()))
-            {
-                msg("Blocked. Yes, I mean blocked.", ChatColor.RED);
-                return true;
-            }
-
-            if (plugin.cb.isCommandBlocked(outCommand, sender))
-            {
-                return true;
-            }
-
-            try
-            {
-                msg("Sending command as " + player.getName() + ": " + outCommand);
-                if (server.dispatchCommand(player, outCommand))
-                {
-                    msg("Command sent.");
-                    player.sendMessage(sender.getName() + " just ran the command: /" + outCommand + " as you!");
-                }
-                else
-                {
-                    msg("Unknown error sending command.");
-                }
-            }
-            catch (Throwable ex)
-            {
-                msg("Error sending command: " + ex.getMessage());
+                msg("Unknown error sending command.");
             }
         }
-        catch (Exception e)
+        catch (Throwable ex)
         {
-            msg("/gcmd <fromname> <outcommand>");
+            msg("Error sending command: " + ex.getMessage());
         }
-        return true;
 
+        return true;
     }
 }
