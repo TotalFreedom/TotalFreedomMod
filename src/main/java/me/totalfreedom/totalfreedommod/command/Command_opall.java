c2f539c29d6deecdac1f24824cff00a2f5fe3400
Commodore64x <Commodore64x@users.noreply.github.com>
2016-11-23 17:00:01 +1000

    Simplified if statement #1922

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_opall.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_opall.java
@@ -38,12 +38,9 @@ public class Command_opall extends FreedomCommand
             player.setOp(true);
             player.sendMessage(FreedomCommand.YOU_ARE_OP);
 
-            if (!plugin.al.isAdmin(player))
+            if (!plugin.al.isAdmin(player) && doSetGamemode)
             {
-                if (doSetGamemode)
-                {
-                    player.setGameMode(targetGamemode);
-                }
+                player.setGameMode(targetGamemode);
             }
         }
 
