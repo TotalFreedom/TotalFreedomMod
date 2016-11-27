8a8f2d13fef1460f39125223c37023ae94f5201c
OutOfYourMind <outofyourmindyt@gmail.com>
2016-11-27 13:41:16 -0500

    Revert "Update NMS to 1.11 and Fix Build Properties"

--- src/me/unraveledmc/unraveledmcmod/ServerInterface.java
+++ src/me/unraveledmc/unraveledmcmod/ServerInterface.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 import java.util.List;
 import me.unraveledmc.unraveledmcmod.util.FLog;
 import me.unraveledmc.unraveledmcmod.util.FUtil;
-import net.minecraft.server.v1_11_R1.EntityPlayer;
-import net.minecraft.server.v1_11_R1.MinecraftServer;
-import net.minecraft.server.v1_11_R1.PropertyManager;
+import net.minecraft.server.v1_10_R1.EntityPlayer;
+import net.minecraft.server.v1_10_R1.MinecraftServer;
+import net.minecraft.server.v1_10_R1.PropertyManager;
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_11_R1.CraftServer;
+import org.bukkit.craftbukkit.v1_10_R1.CraftServer;
 
 public class ServerInterface extends FreedomService
 {
 
-    public static final String COMPILE_NMS_VERSION = "v1_11_R1";
+    public static final String COMPILE_NMS_VERSION = "v1_10_R1";
 
     public ServerInterface(UnraveledMCMod plugin)
     {
--- src/me/unraveledmc/unraveledmcmod/command/Command_totalfreedommod.java
+++ src/me/unraveledmc/unraveledmcmod/command/Command_totalfreedommod.java
@@ -19,15 +19,22 @@ public class Command_totalfreedommod extends FreedomCommand
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
+        UnraveledMCMod.BuildProperties build = UnraveledMCMod.build;
         msg("TotalFreedomMod for 'Total Freedom', the original all-op server.", ChatColor.GOLD);
         msg("Running on " + ConfigEntry.SERVER_NAME.getString() + ".", ChatColor.GOLD);
         msg("Created by Madgeek1450 and Prozza.", ChatColor.GOLD);
-        msg(String.format("Version " + UnraveledMCMod.pluginVersion), ChatColor.GOLD);
+        msg(String.format("Version "
+                + ChatColor.BLUE + "%s %s.%s " + ChatColor.GOLD + "("
+                + ChatColor.BLUE + "%s" + ChatColor.GOLD + ")",
+                build.codename,
+                build.version,
+                build.number,
+                build.head), ChatColor.GOLD);
         msg(String.format("Compiled "
                 + ChatColor.BLUE + "%s" + ChatColor.GOLD + " by "
                 + ChatColor.BLUE + "%s",
-                UnraveledMCMod.buildDate,
-                UnraveledMCMod.compiledBy), ChatColor.GOLD);
+                build.date,
+                build.author), ChatColor.GOLD);
         msg("Visit " + ChatColor.AQUA + "http://github.com/TotalFreedom/TotalFreedomMod"
                 + ChatColor.GREEN + " for more information.", ChatColor.GREEN);
 
