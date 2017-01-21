a57e04ffc22ada97a71d63ca53c3526b1680b03e
CreeperSeth <creeperseth@mail.com>
2017-01-21 11:04:29 -0700

    Modified because I had to remove the build.properties function

--- src/main/java/me/unraveledmc/unraveledmcmod/command/Command_totalfreedommod.java
+++ src/main/java/me/unraveledmc/unraveledmcmod/command/Command_totalfreedommod.java
@@ -19,22 +19,21 @@ public class Command_totalfreedommod extends FreedomCommand
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        UnraveledMCMod.BuildProperties build = UnraveledMCMod.build;
         msg("TotalFreedomMod for 'Total Freedom', the original all-op server.", ChatColor.GOLD);
         msg("Running on " + ConfigEntry.SERVER_NAME.getString() + ".", ChatColor.GOLD);
         msg("Created by Madgeek1450 and Prozza.", ChatColor.GOLD);
         msg(String.format("Version "
                 + ChatColor.BLUE + "%s %s.%s " + ChatColor.GOLD + "("
                 + ChatColor.BLUE + "%s" + ChatColor.GOLD + ")",
-                build.codename,
-                build.version,
-                build.number,
-                build.head), ChatColor.GOLD);
+                null,
+                null,
+                null,
+                null), ChatColor.GOLD);
         msg(String.format("Compiled "
                 + ChatColor.BLUE + "%s" + ChatColor.GOLD + " by "
                 + ChatColor.BLUE + "%s",
-                build.date,
-                build.author), ChatColor.GOLD);
+                null,
+                null), ChatColor.GOLD);
         msg("Visit " + ChatColor.AQUA + "http://github.com/TotalFreedom/TotalFreedomMod"
                 + ChatColor.GREEN + " for more information.", ChatColor.GREEN);
 
