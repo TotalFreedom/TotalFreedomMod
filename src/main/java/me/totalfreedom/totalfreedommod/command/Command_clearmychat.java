021a1e6137826c9dd50873a49457b230df2cc369
Savnith <savnith@gmail.com>
2017-03-27 22:05:24 -0400

    Clearmychat
    https://github.com/TotalFreedom/TotalFreedomMod/issues/1988

---
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_clearmychat.java
@@ -0,0 +1,23 @@
+package me.totalfreedom.totalfreedommod.command;
+
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = Rank.OP, source = SourceType.BOTH)
+@CommandParameters(description = "This is a for the OP's that want to clear there chat", usage = "/<command>", aliases = "cmc")
+public class Command_clearmychat extends F_Command
+{
+    @Override
+    protected boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        for (int i = 0; i <= 150; i++)
+        {
+            msg("");
+        }
+        msg(ChatColor.RED + "You have cleared your own chat!");
+        return true;
+    }
+}
