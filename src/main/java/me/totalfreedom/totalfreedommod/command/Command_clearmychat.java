7e2fc3dae8a597e429891e7b06ae842bd884dc1e
Savnith <savnith@gmail.com>
2017-03-28 01:04:45 -0400

    Update Command_clearmychat.java

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_clearmychat.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_clearmychat.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.OP, source = SourceType.BOTH)
 @CommandParameters(description = "This is a for the OP's that want to clear there chat", usage = "/<command>", aliases = "cmc")
-public class Command_clearmychat extends F_Command
+public class Command_clearmychat extends FreedomCommand
 {
     @Override
     protected boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
@@ -17,7 +17,7 @@ public class Command_clearmychat extends F_Command
         {
             msg("");
         }
-        msg("You have cleared your own chat!", ChatColor.RED);
+        msg("You have cleared your own chat", ChatColor.RED);
         return true;
     }
 }
