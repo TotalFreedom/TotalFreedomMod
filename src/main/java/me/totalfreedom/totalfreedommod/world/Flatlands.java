aabf0c009abe3c0ecac35b379369bb44dc0ccd97
Pramire <pramire@users.noreply.github.com>
2017-06-01 12:51:44 -0400

    Update Flatlands.java

--- src/main/java/me/totalfreedom/totalfreedommod/world/Flatlands.java
+++ src/main/java/me/totalfreedom/totalfreedommod/world/Flatlands.java
@@ -52,10 +52,10 @@ public class Flatlands extends CustomWorld
         org.bukkit.material.Sign signData = (org.bukkit.material.Sign) welcomeSign.getData();
         signData.setFacingDirection(BlockFace.NORTH);
 
-        welcomeSign.setLine(0, ChatColor.GREEN + "Flatlands");
-        welcomeSign.setLine(1, ChatColor.DARK_GRAY + "---");
-        welcomeSign.setLine(2, ChatColor.YELLOW + "Spawn Point");
-        welcomeSign.setLine(3, ChatColor.DARK_GRAY + "---");
+        welcomeSign.setLine(0, ChatColor.DARK_AQUA + "LambdaMinecraft");
+        welcomeSign.setLine(1, ChatColor.WHITE + "Flatlands");
+        welcomeSign.setLine(2, ChatColor.YELLOW + "");
+        welcomeSign.setLine(3, ChatColor.DARK_GRAY + "");
         welcomeSign.update();
 
         plugin.gr.commitGameRules();
