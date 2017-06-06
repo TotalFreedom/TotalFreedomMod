b68430cd3f04f55dab2caf17f1bdeaf100458d74
kobetricee <kobetricee@gmail.com>
2017-06-05 21:53:38 -0400

    Add's a cooldown for /report
    Just a small update, It blocks players from spamming /report to admins [Works tested on mc.frostedop.net]

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_report.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_report.java
@@ -1,8 +1,10 @@
 package me.totalfreedom.totalfreedommod.command;
 
+import java.util.ArrayList;
 import me.totalfreedom.totalfreedommod.rank.Rank;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -12,10 +14,19 @@ import org.bukkit.entity.Player;
 @CommandParameters(description = "Report a player for admins to see.", usage = "/<command> <player> <reason>")
 public class Command_report extends FreedomCommand
 {
-
+    public static ArrayList<Player> cooldown = new ArrayList<Player>();
+    
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
+        final Player p = (Player) sender;
+        
+        if (cooldown.contains(p))
+        {
+            p.sendMessage(ChatColor.RED + "You must wait 10 minutes to make another report.");
+            return true;
+        }
+        
         if (args.length < 2)
         {
             return false;
@@ -48,6 +59,18 @@ public class Command_report extends FreedomCommand
         plugin.cm.reportAction(playerSender, player, report);
 
         msg(ChatColor.GREEN + "Thank you, your report has been successfully logged.");
+        
+        // Begin cooldown
+        cooldown.add(p);
+        
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable()
+        {
+            public void run()
+            {
+                p.sendMessage(ChatColor.GRAY + "You may now make another report.");
+                cooldown.remove(p);
+            }
+        }, 600L * 20L); 
 
         return true;
     }
