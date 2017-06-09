259142f703a4ed9266509a7a071c1496cdf7c43b
Lemon <minecraftoxlemonxo@gmail.com>
2017-06-09 18:16:43 +0500

    Add rainbow commands

---
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_rainbowtag.java
@@ -0,0 +1,44 @@
+package me.totalfreedom.totalfreedommod.command;
+
+import java.util.Iterator;
+import static me.totalfreedom.totalfreedommod.command.Command_tag.FORBIDDEN_WORDS;
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import me.totalfreedom.totalfreedommod.util.FUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = Rank.OP, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Gives you a rainbow tag", usage = "/<command> <tag>", aliases = "tn")
+public class Command_rainbowtag extends FreedomCommand
+{
+
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length < 1)
+        {
+            return false;
+        }
+
+        final String tag = FUtil.rainbowify(ChatColor.stripColor(FUtil.colorize(StringUtils.join(args, " "))));
+        
+        for (String word : FORBIDDEN_WORDS) 
+        {
+            if (tag.contains(word))
+            {
+                msg("That tag contains a forbidden word.");
+                return true;
+            }
+        }
+
+        plugin.pl.getPlayer(playerSender).setTag(tag);
+  
+        msg("Set tag to " + tag);
+
+        return true;
+    }
+}
+    
