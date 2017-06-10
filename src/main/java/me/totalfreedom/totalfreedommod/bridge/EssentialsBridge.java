1afee274a2c601f81d13474d94f9d2b7c24b220c
Commodore64x <Commodore64x@users.noreply.github.com>
2017-06-10 21:31:16 +1000

    Removed Invsee and Vanish Listeners

--- src/main/java/me/totalfreedom/totalfreedommod/bridge/EssentialsBridge.java
+++ src/main/java/me/totalfreedom/totalfreedommod/bridge/EssentialsBridge.java
@@ -4,26 +4,11 @@ import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.User;
 import me.totalfreedom.totalfreedommod.FreedomService;
 import me.totalfreedom.totalfreedommod.TotalFreedomMod;
-import me.totalfreedom.totalfreedommod.player.FPlayer;
-import me.totalfreedom.totalfreedommod.rank.Rank;
 import me.totalfreedom.totalfreedommod.util.FLog;
 import me.totalfreedom.totalfreedommod.util.FUtil;
 import net.ess3.api.IUser;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.HumanEntity;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.potion.PotionEffectType;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class EssentialsBridge extends FreedomService
 {
@@ -176,107 +161,4 @@ public class EssentialsBridge extends FreedomService
 
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onInventoryClickEvent(final InventoryClickEvent event)
-    {
-        Player refreshPlayer = null;
-        final Inventory top = event.getView().getTopInventory();
-        final InventoryType type = top.getType();
-        final Player playerdata = (Player) event.getWhoClicked();
-        FPlayer fPlayer = plugin.pl.getPlayer(playerdata);
-        if (type == InventoryType.PLAYER && fPlayer.isInvsee())
-        {
-            final InventoryHolder invHolder = top.getHolder();
-            if (invHolder != null && invHolder instanceof HumanEntity)
-            {
-                final Player invOwner = (Player) invHolder;
-                final Rank recieverRank = plugin.rm.getRank(playerdata);
-                final Rank playerRank = plugin.rm.getRank(invOwner);
-                if (playerRank.ordinal() >= recieverRank.ordinal() || !(invOwner.isOnline()))
-                {
-                    event.setCancelled(true);
-                    refreshPlayer = playerdata;
-                }
-            }
-        }
-
-        if (refreshPlayer != null)
-        {
-            final Player player = refreshPlayer;
-            new BukkitRunnable()
-            {
-
-                @Override
-                public void run()
-                {
-                    player.updateInventory();
-                }
-
-            }.runTaskLater(this.plugin, 20);
-        }
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onInventoryCloseEvent(final InventoryCloseEvent event)
-    {
-        Player refreshPlayer = null;
-        final Inventory top = event.getView().getTopInventory();
-        final InventoryType type = top.getType();
-        final Player playerdata = (Player) event.getPlayer();
-        FPlayer fPlayer = plugin.pl.getPlayer(playerdata);
-        if (type == InventoryType.PLAYER && fPlayer.isInvsee())
-        {
-            fPlayer.setInvsee(false);
-            refreshPlayer = playerdata;
-        }
-
-        if (refreshPlayer != null)
-        {
-            final Player player = refreshPlayer;
-            new BukkitRunnable()
-            {
-
-                @Override
-                public void run()
-                {
-                    player.updateInventory();
-                }
-
-            }.runTaskLater(this.plugin, 20);
-        }
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerJoin(PlayerJoinEvent event)
-    {
-        final Player player = event.getPlayer();
-
-        for (Player vanishedPlayers : server.getOnlinePlayers())
-        {
-            final FPlayer fPlayer = plugin.pl.getPlayer(vanishedPlayers);
-            if (fPlayer.isVanish())
-            {
-                player.hidePlayer(vanishedPlayers);
-            }
-        }
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerQuit(PlayerQuitEvent event)
-    {
-        final Player player = event.getPlayer();
-        final FPlayer fPlayer = plugin.pl.getPlayer(player);
-        if (fPlayer.isVanish())
-        {
-
-            FLog.info(player.getName() + " is no longer vanished.");
-            for (Player allPlayers : server.getOnlinePlayers())
-            {
-                player.showPlayer(allPlayers);
-            }
-            plugin.esb.setVanished(player.getName(), false);
-            player.removePotionEffect(PotionEffectType.INVISIBILITY);
-            fPlayer.setVanish(false);
-        }
-    }
 }
