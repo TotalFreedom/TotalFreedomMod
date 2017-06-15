531518a69be92f7a3d1bdccf1993ba18b5be8386
CreeperSeth <creeperseth@mail.com>
2017-06-15 15:10:46 -0700

    Implement a CoreProtect bridge

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_wipeflatlands.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_wipeflatlands.java
@@ -6,6 +6,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 @CommandPermissions(level = Rank.SENIOR_ADMIN, source = SourceType.ONLY_CONSOLE, blockHostConsole = true)
 @CommandParameters(description = "Wipe the flatlands map. Requires manual restart after command is used.", usage = "/<command>")
@@ -24,7 +25,21 @@ public class Command_wipeflatlands extends FreedomCommand
             player.kickPlayer("Server is going offline for flatlands wipe, come back in a few minutes.");
         }
 
-        server.shutdown();
+        if (!plugin.cpb.isEnabled())
+        {
+            server.shutdown();
+        }
+        else
+        {
+            new BukkitRunnable()
+            {
+                @Override
+                public void run()
+                {
+                    plugin.cpb.clearDatabase(plugin.wm.flatlands.getWorld(), true);
+                }
+            }.runTaskAsynchronously(plugin);
+        }
 
         return true;
     }
