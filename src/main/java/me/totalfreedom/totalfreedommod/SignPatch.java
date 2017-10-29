212b367c3c1fafc1b41c16f8c917bd55d1d701b9
Lemon <OxLemonxO@users.noreply.github.com>
2017-10-29 19:45:38 +0500

    Update

--- src/main/java/me/totalfreedom/totalfreedommod/SignPatch.java
+++ src/main/java/me/totalfreedom/totalfreedommod/SignPatch.java
@@ -3,11 +3,14 @@ package me.totalfreedom.totalfreedommod;
 import net.minecraft.server.v1_12_R1.NBTTagCompound;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_12_R1.block.CraftBlock;
 import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class SignPatch extends FreedomService
@@ -49,6 +52,17 @@ public class SignPatch extends FreedomService
                 player.sendMessage(ChatColor.GRAY + "You are not allowed to place command signs.");
                 event.setCancelled(true);
             }
+        }
+    }
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerInteractSign(PlayerInteractEvent event)
+    {
+        if(event.getAction() != Action.RIGHT_CLICK_BLOCK)
+         return;
+
+        if (event.getClickedBlock() != null && event.getClickedBlock().getType().equals(Material.SIGN) || event.getClickedBlock().getType().equals(Material.SIGN_POST) || event.getClickedBlock().getType().equals(Material.WALL_SIGN))
+        {
+            event.setCancelled(true);
         }
     }
 }
