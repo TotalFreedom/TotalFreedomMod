d5a37428492aa8ee7e0b5edc65c291f8e25194cf
ZeroEpoch1969 <13510767+ZeroEpoch1969@users.noreply.github.com>
2018-03-24 23:08:50 -0700

    Forgot something

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_verifyplayer.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_verifyplayer.java
@@ -9,19 +9,24 @@ import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH)
 @CommandParameters(description = "Manually verifies a player", usage = "/<command> <partialname>")
-public class Command_verifyplayer extends FreedomCommand {
+public class Command_verifyplayer extends FreedomCommand
+{
 
     @Override
-    public boolean run(final CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole) {
-        if (args.length == 0) {
+    public boolean run(final CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 0)
+        {
             return false;
         }
         final Player player = getPlayer(args[0]);
-        if (player == null) {
+        if (player == null)
+        {
             msg(FreedomCommand.PLAYER_NOT_FOUND, ChatColor.RED);
             return true;
         }
-        if (!plugin.pv.isPlayerImpostor(player)) {
+        if (!plugin.pv.isPlayerImpostor(player))
+        {
             msg("That player is not an impostor.");
             return true;
         }
