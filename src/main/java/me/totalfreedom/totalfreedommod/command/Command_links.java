713ec7c8d9ea033b7603aaef7d3de2f923cb0c43
Mafrans <malte.kluft@gmail.com>
2018-06-01 19:35:48 +0200

    Add a command that shows social media links. Resolves #67

---
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_links.java
@@ -0,0 +1,39 @@
+package me.totalfreedom.totalfreedommod.command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Command_links extends FreedomCommand {
+    @Override
+    protected boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole) {
+        ConfigurationSection section = plugin.getConfig().getConfigurationSection("social_links");
+        Map<String,Object> values = section.getValues(false);
+
+        List<String> lines = new ArrayList<>();
+
+        for(String key : values.keySet()) {
+            if(!(values.get(key) instanceof String)) {
+                continue;
+            }
+
+            String link = (String) values.get(key);
+
+            lines.add(ChatColor.GOLD + "- " + key + ": " + ChatColor.AQUA + link);
+        }
+        if(lines.size() == 0) {
+            lines.add(ChatColor.GOLD + "- There are no links currently added in the config");
+        }
+
+        lines.add(ChatColor.AQUA + "TotalFreedom Social Media Links:");
+        sender.sendMessage(lines.toArray(new String[0]));
+
+        return true;
+    }
+}
