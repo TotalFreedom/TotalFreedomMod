f0a9b6d74790e5c4a2e8ba148605da3306542d62
Lemon <OxLemonxO@users.noreply.github.com>
2018-07-19 16:30:08 +0500

    there's no semicolon

--- src/main/java/me/totalfreedom/totalfreedommod/ChatManager.java
+++ src/main/java/me/totalfreedom/totalfreedommod/ChatManager.java
@@ -161,7 +161,7 @@ public class ChatManager extends FreedomService
                 }
                 else
                 {
-                    player.sendMessage("[" + ChatColor.AQUA + "ADMIN" + ChatColor.WHITE + "] " + ChatColor.DARK_RED + sender.getName() + ChatColor.DARK_GRAY + " [" + getColoredTag(admin, display); + ChatColor.DARK_GRAY + "]" + ChatColor.WHITE + ": " + ChatColor.GOLD + FUtil.colorize(message));
+                    player.sendMessage("[" + ChatColor.AQUA + "ADMIN" + ChatColor.WHITE + "] " + ChatColor.DARK_RED + sender.getName() + ChatColor.DARK_GRAY + " [" + getColoredTag(admin, display) + ChatColor.DARK_GRAY + "]" + ChatColor.WHITE + ": " + ChatColor.GOLD + FUtil.colorize(message));
                 }
             }
         }
