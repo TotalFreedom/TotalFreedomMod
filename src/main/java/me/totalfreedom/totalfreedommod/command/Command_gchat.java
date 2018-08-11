3895c2235e349f16f7ac830a1d87254cbd3c47b3
ZeroEpoch1969 <13510767+ZeroEpoch1969@users.noreply.github.com>
2018-08-11 02:11:03 -0700

    Geek chat

---
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_gchat.java
@@ -0,0 +1,37 @@
+package me.totalfreedom.totalfreedommod.command;
+
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH, blockHostConsole = true)
+@CommandParameters(description = "Send a chat message as someone else.", usage = "/<command> <fromname> <outmessage>")
+public class Command_gchat extends FreedomCommand
+{
+
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length < 2)
+        {
+            return false;
+        }
+
+        final Player player = getPlayer(args[0]);
+
+        if (player == null)
+        {
+            sender.sendMessage(FreedomCommand.PLAYER_NOT_FOUND);
+            return true;
+        }
+
+        final String outMessage = StringUtils.join(args, " ", 1, args.length);
+        msg("Sending message as " + player.getName() + ": " + outMessage);
+        player.chat(outMessage);
+        msg("Message sent.");
+
+        return true;
+    }
+}
