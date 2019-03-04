28e4cf9797fbdadc7f312806b667cbd27509526d
Ryan Wild <ryan-a-wild@protonmail.com>
2019-03-04 22:07:38 +0000

    Worked to make TFM build properly in IntelliJ due to unstable Netbeans versions and look to have also fixed the issue with BStats

--- src/main/java/me/totalfreedom/totalfreedommod/ServerInterface.java
+++ src/main/java/me/totalfreedom/totalfreedommod/ServerInterface.java
@@ -6,7 +6,7 @@ import me.totalfreedom.totalfreedommod.util.FUtil;
 public class ServerInterface extends FreedomService
 {
 
-    public static final String COMPILE_NMS_VERSION = "v1_13_R1";
+    public static final String COMPILE_NMS_VERSION = "v1_13_R2";
 
     public ServerInterface(TotalFreedomMod plugin)
     {
--- src/main/java/me/totalfreedom/totalfreedommod/TotalFreedomMod.java
+++ src/main/java/me/totalfreedom/totalfreedommod/TotalFreedomMod.java
@@ -2,7 +2,6 @@ package me.totalfreedom.totalfreedommod;
 
 import me.totalfreedom.totalfreedommod.fun.Trailer;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import me.totalfreedom.totalfreedommod.admin.AdminList;
