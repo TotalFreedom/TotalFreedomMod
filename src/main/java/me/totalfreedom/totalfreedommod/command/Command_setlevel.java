fd809a0d33c06a7cbc031c48b5c788ed6d86e04a
Will Swoveland <will.arctic@gmail.com>
2019-11-20 21:13:56 -0800

    Grammar fix for command_setlevel.java (#156)
    I catch these as I edit my own fork, I've made one or two other error pulls. If these are annoying or unnecessary, let me know

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_setlevel.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_setlevel.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.OP, source = SourceType.ONLY_IN_GAME)
-@CommandParameters(description = "Sets your expierence level.", usage = "/<command> [level]")
+@CommandParameters(description = "Sets your experience level.", usage = "/<command> [level]")
 public class Command_setlevel extends FreedomCommand
 {
 
