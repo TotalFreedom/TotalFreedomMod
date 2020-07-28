8e8daa457bdd8b0afc22224659e705103218708a
speed <43330808+speedxx@users.noreply.github.com>
2020-07-27 23:37:10 -0400

    fix clearchat

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_clearchat.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_clearchat.java
@@ -1,28 +1,30 @@
 package me.totalfreedom.totalfreedommod.command;
 
-import java.util.stream.IntStream;
 import me.totalfreedom.totalfreedommod.rank.Rank;
+import me.totalfreedom.totalfreedommod.util.FUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = Rank.SUPER_ADMIN, source = SourceType.BOTH)
-@CommandParameters(description = "Clears the chat for players who are not opt-out.", usage = "/<command>", aliases = "cc")
+@CommandParameters(description = "Clears the chat.", usage = "/<command>", aliases = "cc")
 public class Command_clearchat extends FreedomCommand
 {
 
     @Override
     public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-
         for (Player player : server.getOnlinePlayers())
         {
             if (!plugin.al.isAdmin(player))
             {
-                IntStream.range(0, 100).mapToObj(i -> "").forEach(player::sendMessage);
+                for (int i = 0; i < 100; i++)
+                {
+                    player.sendMessage("");
+                }
             }
         }
-
+        FUtil.adminAction(sender.getName(), "Cleared chat", true);
         return true;
     }
 }
