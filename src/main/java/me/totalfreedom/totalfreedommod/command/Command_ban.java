8628e88e3ab5bbda6f3cd2a4efc6d31d2ebc0ebd
DragonSlayer2189 <59424467+DragonSlayer2189@users.noreply.github.com>
2020-08-14 00:29:50 -0400

    added "banned (user) quietly" message
    did i do this right, idk java

--- src/main/java/me/totalfreedom/totalfreedommod/command/Command_ban.java
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_ban.java
@@ -104,7 +104,10 @@ public class Command_ban extends FreedomCommand
                     }
                 }
             }
-
+            else
+            {
+                sender.sendMessage(ChatColor.GRAY + "Banned " + player.getName() + " quietly.");
+            }
             // Kill player
             player.setHealth(0.0);
         }
